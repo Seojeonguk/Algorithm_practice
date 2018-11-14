d34ccbd65ca1d8f1abf963ec6c34c69b7272fc6f
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2018-11-14 18:27:32 +0900

    Revert "Revert "Update BOJ/14241.cpp""
    This reverts commit 04f026f2a376ec74669c1aa62efe4b7d48c79bb0.

---
+++ BOJ/14241.cpp
@@ -0,0 +1,28 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int main() {
+	priority_queue<int> q;
+	int n, a;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a);
+		q.push(a);
+	}
+	int res = 0;
+	while (q.size() != 1) {
+		int w = q.top();
+		q.pop();
+		int e = q.top();
+		q.pop();
+		res += w*e;
+		q.push(w + e);
+	}
+
+	printf("%d\n", res);
+}
