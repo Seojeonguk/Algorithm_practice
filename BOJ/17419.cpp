aca08cb3f12ad7fba4e2aa93c0ed0fd459e13721
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2019-09-01 19:34:59 +0900

    비트가 넘쳐흘러

---
+++ BOJ/17419.cpp
@@ -0,0 +1,22 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int n, k,cnt;
+
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%1d", &k);
+		if (k == 1) cnt++;
+	}
+	printf("%d\n", cnt);
+}
