829bb0537a39e07c5afe26d516a44d26f30bdf38
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2019-12-26 20:21:46 +0900

    Tri-du

---
+++ BOJ/13597.cpp
@@ -0,0 +1,15 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int a, b;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %d", &a, &b);
+	printf("%d\n", max(a, b));
+}
