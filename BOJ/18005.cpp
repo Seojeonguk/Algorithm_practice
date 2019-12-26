f12e711c280b0047808727e987697e9ccdd64d5f
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2019-12-26 20:54:00 +0900

    Even or Odd?

---
+++ BOJ/18005.cpp
@@ -0,0 +1,17 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &n);
+	if (n % 2) puts("0");
+	else if (n % 4) puts("1");
+	else puts("2");
+}
