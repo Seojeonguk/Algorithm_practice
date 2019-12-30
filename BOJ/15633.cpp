43a13f0f994290662cff40ae3eb6a1cbbcabed7b
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2019-12-31 00:10:01 +0900

    Fan Death

---
+++ BOJ/15633.cpp
@@ -0,0 +1,19 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n, ans;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+		if (n%i == 0)ans += i;
+
+	printf("%d\n", ans*5-24);
+}
