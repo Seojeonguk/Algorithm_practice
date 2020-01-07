62dc60dcd2089363d1a151399a26c12264d9ff85
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-07 23:17:09 +0900

    Shifty Sum

---
+++ BOJ/14682.cpp
@@ -0,0 +1,19 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n, k,ans;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %d", &n, &k);
+
+	for (int i = 0; i <= k; i++,n*=10)
+		ans += n;
+
+	printf("%d\n", ans);
+}
