3d6f6837dddda4684b30e6e8902f12493026e837
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-08 19:12:09 +0900

    Sums

---
+++ BOJ/13073.cpp
@@ -0,0 +1,19 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int p, k, n;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &p);
+
+	for (int i = 0; i < p; i++) {
+		scanf("%d", &n);
+		printf("%d %d %d\n", n*(n + 1) / 2, n*n, n*(n + 1));
+	}
+}
