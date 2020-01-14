45459fe3bc7c9af1541abf86d7b5a60d61c53607
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-15 03:02:16 +0900

    Oddities

---
+++ BOJ/10480.cpp
@@ -0,0 +1,19 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		int x; scanf("%d", &x);
+		printf("%d is %s\n", x, x % 2 ? "odd" : "even");
+	}
+}
