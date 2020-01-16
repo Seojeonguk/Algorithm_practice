ab601e298e37da197de28daa30c84ac9b324e10e
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-16 21:10:42 +0900

    Boss Battle

---
+++ BOJ/15600.cpp
@@ -0,0 +1,15 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &n);
+	printf("%d\n", n < 3 ? 1 : n - 2);
+}
