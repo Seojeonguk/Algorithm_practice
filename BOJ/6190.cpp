7acd031d8fe6010849038bee0103f786ff5d0286
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-17 19:59:53 +0900

    Another Cow Number Game

---
+++ BOJ/6190.cpp
@@ -0,0 +1,20 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n, ans;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &n);
+	while(n!=1) {
+		if (n % 2) n = n * 3 + 1;
+		else n /= 2;
+		ans++;
+	}
+	printf("%d\n", ans);
+}
