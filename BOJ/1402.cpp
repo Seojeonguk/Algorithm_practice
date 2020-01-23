bf8975a0a6ccce01fa57ecc25010553d1c9e7bd4
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-24 04:56:14 +0900

    아무래도이문제는A번난이도인것같다

---
+++ BOJ/1402.cpp
@@ -0,0 +1,15 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int t, a, b;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &t);
+	while (t--) puts("yes");
+}
