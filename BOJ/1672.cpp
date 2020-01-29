e25d7fd196f99c24efdfa75e0652eb51e71dd8e3
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-29 17:54:50 +0900

    DNA 해독

---
+++ BOJ/1672.cpp
@@ -0,0 +1,38 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n;
+char ge[1000004];
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %s", &n, ge);
+
+	for (int i = n - 1; i > 0; i--) {
+		if (ge[i-1] == 'A') {
+			if (ge[i] == 'G') ge[i - 1] = 'C';
+			else if (ge[i] == 'T') ge[i - 1] = 'G';
+			else ge[i - 1] = 'A';
+		}
+		else if (ge[i - 1] == 'G') {
+			if (ge[i] == 'A') ge[i - 1] = 'C';
+			else if (ge[i] == 'C') ge[i - 1] = 'T';
+			else if (ge[i] == 'T') ge[i - 1] = 'A';
+		}
+		else if (ge[i - 1] == 'C') {
+			if (ge[i] == 'A') ge[i - 1] = 'A';
+			else if (ge[i] == 'G') ge[i - 1] = 'T';
+			else if (ge[i] == 'T') ge[i - 1] = 'G';
+		}
+		else {
+			if (ge[i] == 'A' || ge[i] == 'C') ge[i - 1] = 'G';
+			else if (ge[i] == 'G') ge[i - 1] = 'A';
+		}
+	}
+	printf("%c", ge[0]);
+}
