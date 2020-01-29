01669482de1019d9c34e5444fc781a1cbeeae1e8
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-29 15:09:49 +0900

    귀여운 수~ε٩(๑> ₃ <)۶з

---
+++ BOJ/17294.cpp
@@ -0,0 +1,28 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+long long int k;
+int tmp[20], idx;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%lld", &k);
+	for (idx = 0; k; idx++) {
+		tmp[idx] = k % 10;
+		k /= 10;
+	}
+	int d = tmp[1] - tmp[0];
+
+	for(int i=0;i<idx-1;i++)
+		if (tmp[i + 1] - tmp[i] != d) {
+			puts("흥칫뿡!! <(￣ ﹌ ￣)>");
+			return 0;
+		}
+
+	puts("◝(⑅•ᴗ•⑅)◜..°♡ 뀌요미!!");
+}
