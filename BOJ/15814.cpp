00fea7f8f608096d533a63f6673c3682bd77e0e2
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-29 17:40:49 +0900

    야바위 대장

---
+++ BOJ/15814.cpp
@@ -0,0 +1,21 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+char name[104];
+int t, a, b;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf(" %s %d", name, &t);
+
+	for (int i = 0; i < t; i++) {
+		scanf("%d %d", &a, &b);
+		swap(name[a], name[b]);
+	}
+	puts(name);
+}
