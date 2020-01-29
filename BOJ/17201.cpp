338c566e4cd00abb74357fe8b7d79402f2e3ed14
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-29 17:25:11 +0900

    자석 체인

---
+++ BOJ/17201.cpp
@@ -0,0 +1,23 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+set<int> s;
+int n, k;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %d", &n, &k);
+
+	while (k) {
+		s.insert(k % 100);
+		k /= 100;
+	}
+
+	if (s.size() == 1) puts("Yes");
+	else puts("No");
+}
