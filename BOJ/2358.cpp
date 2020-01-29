a673a6979564046cf9c0ddf37fb38da28e40c069
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-29 19:40:23 +0900

    평행선

---
+++ BOJ/2358.cpp
@@ -0,0 +1,21 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n, x, y, ans;
+map<int, int> a, b;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &x, &y);
+		if (++a[x] == 2) ans++;
+		if (++b[y] == 2) ans++;
+	}
+	printf("%d\n", ans);
+}
