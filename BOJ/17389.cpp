f227a8325c57415ef59f0a3ae47fd3ce8ff30b3e
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-29 17:04:07 +0900

    보너스 점수

---
+++ BOJ/17389.cpp
@@ -0,0 +1,21 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n,ans,bonus;
+char S[10005];
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %s", &n, S);
+
+	for (int i = 1; i <= n; i++) {
+		if (S[i-1] == 'O') ans += i + bonus++;
+		else bonus = 0;
+	}
+	printf("%d\n", ans);
+}
