fc610e4981061d116505c30b4ea28a3a3a42cfa4
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-29 17:38:50 +0900

    중복된 숫자

---
+++ BOJ/15719.cpp
@@ -0,0 +1,21 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+long long int sum;
+int n, k;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &k);
+		sum += k;
+	}
+	printf("%lld\n", sum - 1LL*(n - 1)*n / 2);
+}
