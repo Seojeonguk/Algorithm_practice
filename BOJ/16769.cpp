a133b585c27bee05426150219bb450282ae47c98
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-29 19:23:58 +0900

    Mixing Milk

---
+++ BOJ/16769.cpp
@@ -0,0 +1,25 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int now[3], m[3];
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	for (int i = 0; i < 3; i++) scanf("%d %d", &m[i], &now[i]);
+
+	for (int i = 0; i < 100; i++) {
+		now[(i+1) % 3] += now[i % 3];
+		if (m[(i+1) % 3] < now[(i+1) % 3]) {
+			now[i % 3] = now[(i + 1) % 3] - m[(i + 1) % 3];
+			now[(i+1) % 3] = m[(i+1) % 3];
+		}
+		else now[i % 3] = 0;
+	}
+
+	for (int i = 0; i < 3; i++) printf("%d\n", now[i]);
+}
