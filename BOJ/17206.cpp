446d830aeaa74e734a6404d33c137e1d32f43b22
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-29 18:32:14 +0900

    준석이의 수학 숙제

---
+++ BOJ/17206.cpp
@@ -0,0 +1,22 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int t, n,dp[80002];
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &t);
+
+	for (int i = 3; i <= 80000; i++)
+		dp[i] = dp[i - 1] + (i % 3 == 0 || i % 7 == 0 ? i : 0);
+
+	while (t--) {
+		scanf("%d", &n);
+		printf("%d\n", dp[n]);
+	}
+}
