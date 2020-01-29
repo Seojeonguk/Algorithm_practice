36c67b7f331310e691f2ba4d723c8a273199c37e
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-29 18:09:29 +0900

    친구 친구

---
+++ BOJ/10865.cpp
@@ -0,0 +1,22 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n, m, ans[100003],x,y;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i < m; i++) {
+		scanf("%d %d", &x, &y);
+		ans[x]++, ans[y]++;
+	}
+
+	for (int i = 1; i <= n; i++)
+		printf("%d\n", ans[i]);
+}
