f0a95f5d5234c13d79d761760d10f4ca430d58a3
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-29 16:48:53 +0900

    Spreadsheets

---
+++ BOJ/16265.cpp
@@ -0,0 +1,38 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int t;
+long long int k;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &t);
+
+	while(t--) {
+		scanf("%lld", &k);
+		k--;
+
+		vector<char> v;
+		int cnt = 0;
+		while (k % 26 != 0) {
+			k--;
+			cnt++;
+		}
+		v.push_back(cnt + 'A');
+		while (k / 26) {
+			k /= 26;
+			k--;
+			v.push_back(k%26 + 'A');
+		}
+
+		int s = v.size()-1;
+
+		for (int i = s; i >= 0; i--) printf("%c", v[i]);
+		puts("");
+	}
+}
