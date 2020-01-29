ff65db5822503dcbe1a04cb318e6df9fcf8d2c44
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-29 16:59:54 +0900

    Close Enough Computations

---
+++ BOJ/4727.cpp
@@ -0,0 +1,22 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int c, f, ch, p;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	while (1) {
+		scanf("%d %d %d %d", &c, &f, &ch, &p);
+		if (!c && !f && !ch && !p) break;
+		c *= 10, f *= 10, ch *= 10, p *= 10;
+		int low = (f ? f - 5 : 0) * 9 + (ch ? ch - 5 : 0) * 4 + (p ? p - 5 : 0) * 4;
+		int high = (f + 5) * 9 + (ch + p + 10) * 4;
+		if (low <= c && c <= high) puts("yes");
+		else puts("no");
+	}
+}
