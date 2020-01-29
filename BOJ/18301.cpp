8184c25d559ca5ac7d5dc2540a57babc448b6537
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-29 15:13:18 +0900

    Rats

---
+++ BOJ/18301.cpp
@@ -0,0 +1,16 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n1, n2, n12;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %d %d", &n1, &n2, &n12);
+
+	printf("%d\n", (n1 + 1)*(n2 + 1) / (n12 + 1) - 1);
+}
