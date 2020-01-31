76191912afd3b40937fafc0e68479c3d9adeaaf8
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-01-31 13:09:46 +0900

    캠핑

---
+++ BOJ/4796.cpp
@@ -0,0 +1,19 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int l, p, v,idx;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	while (1) {
+		scanf("%d %d %d", &l, &p, &v);
+		if (!v) break;
+
+		printf("Case %d: %d\n", ++idx,v / p * l + min(v % p,l));
+	}
+}
