5bebc81d6b2e98ba460728eb30417c7cdfb425e9
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-07 21:33:47 +0900

    등차수열에서 항 번호 찾기

---
+++ BOJ/14913.cpp
@@ -0,0 +1,19 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int a, d, k;
+
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %d %d", &a, &d, &k);
+	int tmp = (k - a) / d + 1;
+	if ((k - a) % d == 0 && tmp > 0) printf("%d\n", tmp);
+	else puts("X");
+}
