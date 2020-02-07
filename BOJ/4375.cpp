526977fe6d8d4c2a49e3ee8d7fe8451b9eca1b96
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-07 21:12:18 +0900

    1

---
+++ BOJ/4375.cpp
@@ -0,0 +1,27 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long int ll;
+
+ll x;
+
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	while (scanf("%lld", &x) != EOF) {
+		int cnt = 0;
+		ll now = 0;
+		while (1) {
+			cnt++;
+			now = now * 10 + 1;
+			if (now && now%x == 0) break;
+			now %= x;
+		}
+		printf("%d\n", cnt);
+	}
+}
