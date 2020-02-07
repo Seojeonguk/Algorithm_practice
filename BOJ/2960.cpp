e8e0b098d0538c9591e884bd2962e1b102a9e460
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-07 21:07:14 +0900

    에라토스테네스의 체

---
+++ BOJ/2960.cpp
@@ -0,0 +1,28 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int n, k, cnt;
+bool chk[1004];
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %d", &n, &k);
+
+	for (int i = 2; i <= n; i++)
+		if (!chk[i])
+			for (int j = i; j <= n; j += i)
+				if (!chk[j]) {
+					cnt++;
+					chk[j] = true;
+					if (cnt == k) {
+						printf("%d\n", j);
+						return 0;
+					}
+				}
+}
