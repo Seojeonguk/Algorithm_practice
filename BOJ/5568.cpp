cf9ff61e6528a4241f4348a26f7db0b7aa524f41
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-07 21:15:36 +0900

    카드 놓기

---
+++ BOJ/5568.cpp
@@ -0,0 +1,38 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+bool coll[11];
+int n, card[11], k;
+set<int> s;
+
+void solve(int sum, int cnt) {
+	if (cnt == k) {
+		s.insert(sum);
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (!coll[i]) {
+			coll[i] = true;
+			solve((card[i] >= 10 ? sum * 100 + card[i] : sum * 10 + card[i]), cnt + 1);
+			coll[i] = false;
+		}
+	}
+
+}
+
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %d", &n, &k);
+	for (int i = 0; i < n; i++) scanf("%d", &card[i]);
+
+	solve(0, 0);
+
+	printf("%d\n", s.size());
+}
