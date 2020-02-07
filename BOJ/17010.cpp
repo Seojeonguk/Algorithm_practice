d57cd17f8172912c28361131755b74ee48230a7e
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-07 21:01:20 +0900

    Time to Decompress

---
+++ BOJ/17010.cpp
@@ -0,0 +1,19 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int t, n;
+char c;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	for (scanf("%d", &t); t--;) {
+		scanf("%d %c", &n, &c);
+		for (int i = 0; i < n; i++) printf("%c", c);
+		puts("");
+	}
+}
