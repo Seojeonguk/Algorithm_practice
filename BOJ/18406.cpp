d11057c41ca1b2f0586ea0be1a8d2d28c83d1bc6
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-08 01:44:05 +0900

    럭키 스트레이트

---
+++ BOJ/18406.cpp
@@ -0,0 +1,18 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+string n;
+int chk;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	cin >> n;
+	for (int i = 0; i < n.length(); i++)
+		chk += (i < n.length() / 2 ? n[i] - '0' : -(n[i] - '0'));
+	puts(chk ? "READY" : "LUCKY");
+}
