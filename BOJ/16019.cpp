604880868c69b967ede77df55ea3dfbd57df87df
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-08 01:17:02 +0900

    Are we there yet?

---
+++ BOJ/16019.cpp
@@ -0,0 +1,20 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int chk[6][6];
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	for (int i = 0; i < 4; i++) scanf("%d", &chk[5][i]);
+
+	for (int i = 0; i < 5; i++,puts("")) 
+		for (int j = 0; j < 5; j++) {
+			chk[i][j] = (i < j ? chk[i][j - 1] + chk[5][j - 1] : chk[j][i]);
+			printf("%d ", chk[i][j]);
+		}
+}
