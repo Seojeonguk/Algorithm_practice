2219207b63513909baf22923ca1bfb61dee592a1
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-07 21:08:04 +0900

    소인수분해

---
+++ BOJ/11653.cpp
@@ -0,0 +1,37 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n;
+bool chk[10000001];
+vector<int> v;
+
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &n);
+
+	for (int i = 2; i <= n; i++) {
+		if (!chk[i]) {
+			for (int j = i; j <= n; j++) {
+				chk[j] = true;
+				v.push_back(j);
+			}
+		}
+	}
+
+	int size = v.size();
+	for (int i = 0; n != 1;) {
+		if (n%v[i])
+			i++;
+		else {
+			n /= v[i];
+			printf("%d\n", v[i]);
+		}
+	}
+}
+
