613d5b57f4679a8da8dd49e1b8d1e255d1a0c993
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-07 21:06:57 +0900

    분수 합

---
+++ BOJ/1735.cpp
@@ -0,0 +1,30 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int a[2], b[2];
+
+int gcd(int x, int y) {
+	if (y == 0) return x;
+	return gcd(y, x%y);
+}
+
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %d %d %d", &a[0], &a[1], &b[0], &b[1]);
+	int chk = a[1] * b[1] / gcd(a[1], b[1]);
+	a[0] *= chk / a[1];
+	b[0] *= chk / b[1];
+	a[0] += b[0];
+
+	int last = gcd(a[0], chk);
+
+	printf("%d %d\n", a[0] / last, chk / last);
+
+}
