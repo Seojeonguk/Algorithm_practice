30d1f785f36f24a6a8777513a2dc6a58a944e629
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-08 01:11:57 +0900

    Teleportation

---
+++ BOJ/15751.cpp
@@ -0,0 +1,16 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int a, b, x, y,ans;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %d %d %d", &a, &b, &x, &y);
+
+	printf("%d\n", min(abs(b - a), min(abs(a - x) + abs(b - y), abs(a - y) + abs(x - b))));
+}
