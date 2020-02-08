c49c16bd1190158ac8dd679fa9f2a86c0da64965
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-09 01:24:55 +0900

    Choose your own path

---
+++ BOJ/16021.cpp
@@ -0,0 +1,56 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+bool visit[10002];
+vector<int> v[10002];
+int n,x,y;
+int solve() {
+	queue<pair<int, int> > q;
+	visit[1] = true;
+	q.push({ 1,1 });
+	int ret = 999999999;
+	while (!q.empty()) {
+		auto outdata = q.front();
+		q.pop();
+
+		if (!v[outdata.first].size()) {
+			ret = min(ret, outdata.second);
+			continue;
+		}
+
+		int size = v[outdata.first].size();
+		for (int i = 0; i < size; i++) {
+			if (visit[v[outdata.first][i]]) continue;
+			visit[v[outdata.first][i]] = true;
+			q.push({ v[outdata.first][i],outdata.second + 1 });
+		}
+	}
+	return ret;
+}
+bool chk() {
+	for (int i = 1; i <= n; i++)
+		if (!visit[i]) return false;
+	return true;
+}
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &x);
+		for (int j = 0; j < x; j++) {
+			scanf("%d", &y);
+			v[i].push_back(y);
+		}
+	}
+
+	int tmp = solve();
+	puts(chk() ? "Y" : "N");
+	printf("%d\n", tmp);
+}
