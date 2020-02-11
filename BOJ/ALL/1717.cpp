cd4b36c4dd7740cf54bca02d7cbe5a70b466a615
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-11 20:21:58 +0900

    집합의 표현

---
+++ BOJ/ALL/1717.cpp
@@ -0,0 +1,38 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long int ll;
+ll chk[22], brr[22];
+int n, k, b;
+int par[1000002], note[1000002];
+
+int find(int a) {
+	if (a == par[a]) return a;
+	return par[a] = find(par[a]);
+}
+
+void setting(int a, int b) {
+	int u = find(a), v = find(b);
+	if (note[u] > note[v]) swap(u, v);
+	par[u] = v;
+	if (note[u] == note[v]) note[v]++;
+}
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %d", &n, &k);
+
+	for (int i = 0; i <= n; i++) par[i] = i;
+	int a, b, c;
+	for (int i = 0; i < k; i++) {
+		scanf("%d %d %d", &a, &b, &c);
+
+		if (!a) setting(b, c);
+		else puts(find(b) == find(c) ? "YES" : "NO");
+	}
+}
