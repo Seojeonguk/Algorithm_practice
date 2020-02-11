4f096b6f9c8bdfa3d2a2a38d2ca04d2a1b141cbf
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-11 20:34:23 +0900

    키 순서

---
+++ BOJ/Olympiad/한국정보올림피아드/한국정보올림피아드시․도지역본선/지역본선 2011/초등부/4.cpp
@@ -0,0 +1,40 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+bool chk[501][501];
+int n, m, x, y;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i < m; i++) {
+		scanf("%d %d", &x, &y);
+		chk[x][y] = true;
+	}
+
+	for (int k = 1; k <= n; k++)
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++)
+				chk[i][j] |= (i == j ? true : (chk[i][k] & chk[k][j]));
+		}
+
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+			chk[j][i] |= chk[i][j];
+
+	int ans = n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++)
+			if (!chk[i][j]) {
+				ans--;
+				break;
+			}
+	}
+	printf("%d\n", ans);
+}
