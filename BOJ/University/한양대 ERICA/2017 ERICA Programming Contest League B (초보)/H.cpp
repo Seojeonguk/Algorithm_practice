e1db236af509edca5031b284f8bcaa387d148aa3
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-11 23:10:51 +0900

    거스름돈

---
+++ BOJ/University/한양대 ERICA/2017 ERICA Programming Contest League B (초보)/H.cpp
@@ -0,0 +1,24 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int n;
+
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &n);
+
+	for (int i = n / 5; i >= 0; i--) {
+		if (i * 5 + (n - i * 5) / 2 * 2 == n) {
+			printf("%d\n", i + (n - i * 5) / 2);
+			return 0;
+		}
+	}
+	puts("-1");
+}
