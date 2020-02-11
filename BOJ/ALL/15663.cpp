86ff8ee89bd0883e4d69d45fd2093e287846c610
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-11 20:28:41 +0900

    N과 M (9)

---
+++ BOJ/ALL/15663.cpp
@@ -0,0 +1,38 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n, m, nm[10], chk[10001];
+int ans[10];
+
+void solve(int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) printf("%d ", ans[i]);
+		puts("");
+		return;
+	}
+
+	for (int i = 0; i <= 10000; i++)
+		if (chk[i]) {
+			chk[i]--;
+			ans[cnt] = i;
+			solve(cnt + 1);
+			chk[i]++;
+		}
+}
+
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &nm[i]);
+		chk[nm[i]] = (chk[nm[i]] + 1) >= m ? m : chk[nm[i]] + 1;
+	}
+	solve(0);
+}
