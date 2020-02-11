1fa7ed1232036c97c141c9208e4f6988cdff5932
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-11 22:09:16 +0900

    K진 트리

---
+++ BOJ/ALL/11812.cpp
@@ -0,0 +1,32 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long int ll;
+ll n, k, q, x, y;
+
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%lld %lld %lld", &n,&k,&q);
+
+	for (int i = 0; i < q; i++) {
+		scanf("%lld %lld", &x, &y);
+		if (k == 1) {
+			printf("%lld\n", abs(x - y));
+			continue;
+		}
+		ll ans = 0;
+		while (x != y) {
+			if (x < y) swap(x, y);
+			x = (x - 2) / k + 1;
+			ans++;
+		}
+		printf("%lld\n", ans);
+	}
+}
