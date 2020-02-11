19822bf709bd5184aa27b0febd233770f66e57ca
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-11 20:32:40 +0900

    네트워크 연결

---
+++ BOJ/ALL/1922.cpp
@@ -0,0 +1,43 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n, m, a, b, c, ans;
+int par[1001], note[1001];
+vector<pair<int, int> > v[1001];
+int find(int x) { return par[x] = (par[x] == x) ? par[x] : find(par[x]); }
+void setting(int u, int v) {
+	int x = find(u), y = find(v);
+	if (note[x] > note[y]) swap(x, y);
+	par[x] = y;
+	if (note[x] == note[y]) note[y]++;
+
+}
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i <= n; i++)par[i] = i;
+
+	priority_queue<pair<int, pair<int, int> > > pq;
+	for (int i = 0; i < m; i++) {
+		scanf("%d %d %d", &a, &b, &c);
+		pq.push({ -c,{a,b} });
+	}
+
+	while (!pq.empty()) {
+		auto outdata = pq.top();
+		pq.pop();
+
+		if (find(outdata.second.first) != find(outdata.second.second)) {
+			setting(outdata.second.first, outdata.second.second);
+			ans += -outdata.first;
+		}
+	}
+	printf("%d\n", ans);
+}
