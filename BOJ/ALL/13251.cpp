d890c9049e717595fca2211b33182e6b5d37bce6
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-11 20:27:50 +0900

    조약돌 꺼내기

---
+++ BOJ/ALL/13251.cpp
@@ -0,0 +1,35 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int m, arr[52], k, all;
+double nck[2502][2502];
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &m);
+
+	for (int i = 0; i < m; i++) {
+		scanf("%d", &arr[i]);
+		all += arr[i];
+	}
+
+	scanf("%d", &k);
+
+	nck[0][0] = nck[1][0] = nck[1][1] = 1;
+
+	for (int i = 2; i <= 2500; i++) {
+		nck[i][0] = nck[i][i] = 1;
+		for (int j = 1; j < i; j++)
+			nck[i][j] = nck[i - 1][j - 1] + nck[i - 1][j];
+	}
+
+	double sum = 0;
+	for (int i = 0; i < m; i++) sum += nck[arr[i]][k];
+
+	printf("%.12lf\n", sum / nck[all][k]);
+}
