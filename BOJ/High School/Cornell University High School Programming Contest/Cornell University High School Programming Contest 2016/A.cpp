afdbfddb805866df81921870ed35cfda1d28a069
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-18 15:54:11 +0900

    Small Weird Measurements

---
+++ BOJ/High School/Cornell University High School Programming Contest/Cornell University High School Programming Contest 2016/A.cpp
@@ -0,0 +1,15 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+long long int A, B, C, D;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%lld %lld %lld %lld", &A, &B, &C, &D);
+	printf("%lld\n", (C + D) / (A - B));
+}
