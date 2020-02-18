0ec4c2324bfe1b67bcf3b2c6c052a9037491348d
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-02-18 16:25:13 +0900

    Large PhD Restaurant

---
+++ BOJ/ALL/14012.cpp
@@ -0,0 +1,25 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+long long int m;
+int n;
+pair<int, int> ab[100005];
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d %lld", &n, &m);
+	for (int i = 0; i < n; i++) scanf("%d", &ab[i].first);
+	for (int i = 0; i < n; i++) scanf("%d", &ab[i].second);
+	sort(ab, ab + n);
+
+	for (int i = 0; i < n; i++) {
+		if (ab[i].second <= ab[i].first) continue;
+		if (ab[i].first <= m) m += ab[i].second - ab[i].first;
+	}
+	printf("%lld\n", m);
+}
