bf9b2a0f05fda23b0c1ac5ed82a053a56a3b05ba
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-03-12 16:57:21 +0900

    Cornell Party

---
+++ BOJ/High School/Cornell University High School Programming Contest/2nd Annual Cornell University High School Programming Contest/6.cpp
@@ -0,0 +1,43 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int t, n,x;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &t);
+	while (t--) {
+		bool chk = true;
+		scanf(" %d ", &n);
+		vector<int> v1(1000001);
+		vector<int> v2(1000001);
+		vector<int> ans(1000001);
+
+		for (int i = 0; i < n; i++) {
+			scanf("%d", &x);
+			v1[x]++;
+		}
+
+		for (int i = 0; i < 1000001; i++) ans[v1[i]]++;
+
+		for (int i = 0; i < n; i++) {
+			scanf("%d", &x);
+			v2[x]++;
+		}
+
+		for (int i = 0; i < 1000001; i++) ans[v2[i]]--;
+		for(int i=0;i<1000001;i++) 
+			if (ans[i]) {
+				chk = false;
+				break;
+			}
+
+		if (chk) puts("what a lovely party");
+		else puts("you've messed up, Cornell");
+	}
+}
