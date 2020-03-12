9356f2fdfb63125bd0e18c85c16011ddca523286
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-03-12 17:41:41 +0900

    Report Card Time

---
+++ BOJ/High School/University of Virginia High School Programming Contest/C.cpp
@@ -0,0 +1,53 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+
+int t, grade;
+
+using namespace std;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif]
+	scanf("%d", &t);
+
+	while (t--) {
+		string a;
+		cin >> a;
+		scanf("%d", &grade);
+		cout << a;
+		printf(" ");
+		switch (grade / 10) {
+		case 10:
+			printf("A+");
+			break;
+		case 9:
+			printf("A");
+			if (grade % 10 > 6)
+				printf("+");
+			break;
+		case 8:
+			printf("B");
+			if (grade % 10 > 6)
+				printf("+");
+			break;
+		case 7:
+			printf("C");
+			if (grade % 10 > 6)
+				printf("+");
+			break;
+		case 6:
+			printf("D");
+			if (grade % 10 > 6)
+				printf("+");
+			break;
+		default:
+			printf("F");
+			break;
+		}
+		printf("\n");
+	}
+}
