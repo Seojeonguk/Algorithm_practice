6a70ef4ae6b4b0b5990df9e25b5f3b5e4d858743
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-03-12 17:36:26 +0900

    Anagrams

---
+++ BOJ/ALL/6996.cpp
@@ -0,0 +1,41 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int cnt[26], cnt2[26];
+char arr[101], brr[101];
+int length, length2;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif]
+	int test;
+	cin >> test;
+	while (test--) {
+		bool ok = false;
+		memset(cnt2, 0, sizeof cnt2);
+		memset(cnt, 0, sizeof cnt);
+		cin >> arr >> brr;
+		length = strlen(arr);
+		length2 = strlen(brr);
+
+		if (length != length2) {
+			cout << arr << " " << "&" << " " << brr << " are NOT anagrams.\n";
+			continue;
+		}
+
+		for (int i = 0; i < length; i++) {
+			cnt[arr[i] - 'a']++;
+			cnt2[brr[i] - 'a']++;
+		}
+		for (int i = 0; i < 26; i++)
+			if (cnt[i] != cnt2[i])
+				ok = true;
+
+		if (!ok) cout << arr << " " << "&" << " " << brr << " are anagrams.\n";
+		else cout << arr << " " << "&" << " " << brr << " are NOT anagrams.\n";
+	}
+}
