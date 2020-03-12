043caf440480eb2760c4664b3d8a529ce656e6b0
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-03-12 17:24:27 +0900

    Sharing

---
+++ BOJ/ALL/10178.cpp
@@ -0,0 +1,19 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n,a,b;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d %d", &a, &b);
+		printf("You get %d piece(s) and your dad gets %d piece(s).\n", a / b, a%b);
+	}
+}
