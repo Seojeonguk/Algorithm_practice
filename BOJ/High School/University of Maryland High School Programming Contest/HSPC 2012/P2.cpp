43e39ad3b3c3f20e5156a2393070e611b91121f1
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-03-12 17:38:17 +0900

    Alphabet Distances

---
+++ BOJ/High School/University of Maryland High School Programming Contest/HSPC 2012/P2.cpp
@@ -0,0 +1,28 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int n;
+string a, b;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif]
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		int length = a.length();
+
+		cout << "Distances: ";
+
+		for (int j = 0; j < length; j++) {
+			if (b[j] - a[j] < 0) cout << b[j] - a[j] + 26 << ' ';
+			else cout << b[j] - a[j] << ' ';
+		}
+		cout << endl;
+	}
+}
