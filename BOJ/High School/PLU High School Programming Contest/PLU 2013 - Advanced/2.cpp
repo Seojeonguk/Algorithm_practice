01c1c1e642c51d9ec4e2ee7e2579121eef861141
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-03-12 17:26:54 +0900

    LCM

---
+++ BOJ/High School/PLU High School Programming Contest/PLU 2013 - Advanced/2.cpp
@@ -0,0 +1,26 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+unsigned long long int ans;
+
+long long int GCD(int x, int y) {
+	if (y == 0)	return x;
+	else GCD(y, x%y);
+}
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	int test;
+	cin >> test;
+	while (test--) {
+		unsigned long long int a, b;
+		cin >> a >> b;
+		ans = (a*b) / GCD(a, b);
+		cout << ans << "\n";
+	}
+}
