5ab4cb123ba2a3d886f20a0937d669fae43044f1
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-03-12 17:39:39 +0900

    !terces poT

---
+++ BOJ/ALL/11365.cpp
@@ -0,0 +1,20 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif]
+	char arr[10001];
+	while (1) {
+		gets(arr);
+		if (arr[0] == 'E' && arr[1] == 'N' && arr[2] == 'D')
+			break;
+		reverse(arr, arr + strlen(arr));
+		puts(arr);
+	}
+}
