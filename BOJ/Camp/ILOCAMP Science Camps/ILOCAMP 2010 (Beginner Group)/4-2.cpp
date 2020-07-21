0bea02a5caad5e76ab195cfa2b865dbada5e6ad5
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-07-21 14:58:29 +0900

    Patyki

---
+++ BOJ/Camp/ILOCAMP Science Camps/ILOCAMP 2010 (Beginner Group)/4-2.cpp
@@ -0,0 +1,20 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+int leng[3];
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	for (int i = 0; i < 3; i++) scanf("%d", &leng[i]);
+
+	sort(leng, leng + 3);
+
+	if (leng[0] == leng[2]) puts("2");
+	else if (leng[0] * leng[0] + leng[1] * leng[1] == leng[2] * leng[2]) puts("1");
+	else puts("0");
+}
