17023dec61bd8c07a4f4b6194ce68169751e03a4
Seojeonguk <uk7880@naver.com>
2020-12-29 22:23:23 +0900

    Sir Bedavere’s Bogus Division Solutions

---
+++ BOJ/ALL/9161.cpp
@@ -0,0 +1,20 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	puts("166 / 664 = 16 / 64");
+	puts("199 / 995 = 19 / 95");
+	puts("217 / 775 = 21 / 75");
+	puts("249 / 996 = 24 / 96");
+	puts("266 / 665 = 26 / 65");
+	puts("499 / 998 = 49 / 98");
+}
