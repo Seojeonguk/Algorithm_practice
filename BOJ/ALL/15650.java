0d1da87b08dc699db2411309ab0661641b04c97d
Seojeonguk <uk7880@naver.com>
2021-01-21 18:14:02 +0900

    N과 M(2)

---
+++ BOJ/ALL/15650.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	static int n,m;
+	static int arr[];
+	static boolean chk[];
+	
+	public static void solve (int x,int now) throws Exception {
+		if(x==m) {
+			for(int i=0;i<arr.length;i++) {
+				bw.write(arr[i] + " ");
+			}
+			bw.write("\n");
+			return;
+		}
+		
+		for(int i=now;i<=n;i++) {
+			if(chk[i] == false) {
+				chk[i] = true;
+				arr[x]=i;
+				solve(x+1,i+1);
+				chk[i] = false;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		chk = new boolean[n+1];
+		arr = new int[m];
+		
+		solve(0,1);
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
