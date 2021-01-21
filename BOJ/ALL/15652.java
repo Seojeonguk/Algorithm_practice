223296cc354742abd456bc780426049324ad9087
Seojeonguk <uk7880@naver.com>
2021-01-21 18:36:12 +0900

    N과 M(4)

---
+++ BOJ/ALL/15652.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	static int n,m;
+	static int arr[];
+	
+	public static void solve (int x,int now) throws Exception {
+		if(x==m) {
+			for(int i=0;i<arr.length;i++) {
+				bw.write(arr[i] + " ");
+			}
+			bw.write("\n");
+			return;
+		}
+		
+		for(int i=now;i<=n;i++) {
+				arr[x]=i;
+				solve(x+1,i);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[m];
+		
+		solve(0,1);
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
