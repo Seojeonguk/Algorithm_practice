51a50e3e7b3357bd4b674da6e896420ff089a072
Seojeonguk <uk7880@naver.com>
2021-02-08 17:57:47 +0900

    Minigolf

---
+++ BOJ/ALL/20877.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+
+	static int n,x,ans;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		n = Integer.valueOf(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i=0;i<n;i++) {
+			x = Integer.valueOf(st.nextToken());
+			ans += (x>7?7:x) - (i%2==0 ? 2:3);
+		}
+		
+		bw.write(String.valueOf(ans));
+
+		bw.flush();
+		bw.close();
+	}
+}
