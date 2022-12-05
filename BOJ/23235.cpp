91240f4b0b6d56c1a55df55fba4d74238e20ff9b
Seojeonguk <junguk7880@naver.com>
2022-12-05 23:28:47 +0900

    The Fastest Sorting Algorithm In The World
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/23235.cpp
@@ -0,0 +1,25 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int arrCnt, value;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    for (int testCaseNum = 1;;testCaseNum++) {
+        scanf("%d", &arrCnt);
+        if (!arrCnt) {
+            break;
+        }
+        for (int idx = 0; idx < arrCnt; idx++) {
+            scanf("%d", &value);
+        }
+        printf("Case %d: Sorting... done!\n", testCaseNum);
+    }
+}
