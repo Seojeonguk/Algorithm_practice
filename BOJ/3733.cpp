36b03f9a00c5bbd2bb29d7cf3f3809b6d2cbc0ed
Seojeonguk <junguk7880@naver.com>
2022-12-05 22:34:20 +0900

    Shares
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/3733.cpp
@@ -0,0 +1,18 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int personCnt, shareAmount;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    while (scanf("%d %d", &personCnt, &shareAmount) != EOF) {
+        printf("%d\n", shareAmount / (personCnt + 1));
+    }
+}
