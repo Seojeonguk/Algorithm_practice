a5312dea3392c805e7042bd289d30681be451d0e
Seojeonguk <junguk7880@naver.com>
2022-12-05 21:10:15 +0900

    곰곰이와 시소
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26072.cpp
@@ -0,0 +1,36 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int chickenCnt, seesawlength;
+
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d", &chickenCnt, &seesawlength);
+
+    vector<pair<double, double> > chickens(chickenCnt);
+
+    for (int idx = 0; idx < chickenCnt; idx++) {
+        scanf("%lf", &chickens[idx].first);
+    }
+
+    for (int idx = 0; idx < chickenCnt; idx++) {
+        scanf("%lf", &chickens[idx].second);
+    }
+
+    double weightSum = 0.0f;
+    double multiSum = 0.0f;
+    for (int idx = 0; idx < chickenCnt; idx++) {
+        weightSum += chickens[idx].second;
+        multiSum += (chickens[idx].first * chickens[idx].second);
+    }
+
+    printf("%lf\n", multiSum / weightSum);
+}
