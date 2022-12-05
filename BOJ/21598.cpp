4971c14cbb0d5d862a785cc0c84cb6bd93e9736e
Seojeonguk <junguk7880@naver.com>
2022-12-05 23:11:36 +0900

    SciComLove
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/21598.cpp
@@ -0,0 +1,19 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int bowCnt;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &bowCnt);
+    for (int bow = 0; bow < bowCnt; bow++) {
+        puts("SciComLove");
+    }
+}
