8d82b4a8433c9f9925bb019d0e058769c546cf4e
Seojeonguk <junguk7880@naver.com>
2022-12-06 00:23:46 +0900

    가희와 방어율 무시
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/25238.cpp
@@ -0,0 +1,23 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+double defensiveRate, disregardRate;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%lf %lf", &defensiveRate, &disregardRate);
+    double sensoryDefensiveRate = defensiveRate - (defensiveRate * disregardRate / 100);
+    if (sensoryDefensiveRate >= 100.0f) {
+        puts("0");
+    }
+    else {
+        puts("1");
+    }
+}
