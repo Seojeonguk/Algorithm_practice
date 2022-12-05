a583d55deebac2a069043485f32b2342db5bb3dd
Seojeonguk <junguk7880@naver.com>
2022-12-05 22:56:36 +0900

    Zadanie próbne 2
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/8871.cpp
@@ -0,0 +1,16 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    int round;
+    scanf("%d", &round);
+    printf("%d %d\n", (round + 1) * 2, (round + 1) * 3);
+}
