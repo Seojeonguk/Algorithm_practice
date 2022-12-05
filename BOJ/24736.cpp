c83190c5461e6429742d1752f6b6c889b51d8a24
Seojeonguk <junguk7880@naver.com>
2022-12-05 23:54:03 +0900

    Football Scoring
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/24736.cpp
@@ -0,0 +1,24 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int boxCnt[5];
+int score[5] = { 6,3,2,1,2 };
+int teamScore[2];
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    for (int team = 0; team < 2; team++) {
+        for (int idx = 0; idx < 5; idx++) {
+            scanf("%d", &boxCnt[idx]);
+            teamScore[team] += boxCnt[idx] * score[idx];
+        }
+    }
+    printf("%d %d\n", teamScore[0], teamScore[1]);
+}
