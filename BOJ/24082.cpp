04842fce6b4c2b3caef146dfe03068920f94fcea
Seojeonguk <junguk7880@naver.com>
2022-12-05 23:32:08 +0900

    立方体 (Cube)
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/24082.cpp
@@ -0,0 +1,17 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int length;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &length);
+    printf("%d\n", length * length * length);
+}
