2c6d6c2cdf5940caf32d5af27cd7401a089abbf9
Seojeonguk <junguk7880@naver.com>
2022-12-06 01:57:36 +0900

    영수증
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/25304.cpp
@@ -0,0 +1,27 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int total, thingCnt, money,cnt;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &total);
+    scanf("%d", &thingCnt);
+    for (int thing = 0; thing < thingCnt; thing++) {
+        scanf("%d %d", &money, &cnt);
+        total -= money * cnt;
+    }
+    if (total) {
+        puts("No");
+    }
+    else {
+        puts("Yes");
+    }
+}
