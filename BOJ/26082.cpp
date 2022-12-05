ad1101fbf23bb0e33ae029bec6a80e98a643bb38
Seojeonguk <junguk7880@naver.com>
2022-12-06 02:11:34 +0900

    WARBOY
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26082.cpp
@@ -0,0 +1,18 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int competitorPrice, competitorPerformance, warboyPrice;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d %d", &competitorPrice, &competitorPerformance, &warboyPrice);
+
+    printf("%d\n", competitorPerformance / competitorPrice * 3 * warboyPrice);
+}
