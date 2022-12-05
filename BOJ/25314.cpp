a67d681048579ed016e4bec2f709bafbd1b0b201
Seojeonguk <junguk7880@naver.com>
2022-12-06 02:02:30 +0900

    코딩은 체육과목 입니다
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/25314.cpp
@@ -0,0 +1,21 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int byteCnt;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &byteCnt);
+
+    for (int idx = 0; idx < byteCnt / 4; idx++) {
+        printf("long ");
+    }
+    printf("int\n");
+}
