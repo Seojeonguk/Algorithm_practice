1f866cbf3cc977a973291b1ca4731ebf34c48a3b
Seojeonguk <junguk7880@naver.com>
2022-12-05 23:22:10 +0900

    The World Responds
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/23234.cpp
@@ -0,0 +1,14 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    puts("The world says hello!")
+}
