91c36120e823badfa03effd963a58de89b14a2d2
Seojeonguk <junguk7880@naver.com>
2022-12-06 02:15:44 +0900

    Intercepting Information
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26209.cpp
@@ -0,0 +1,28 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int info;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    bool isCorresponding = false;
+    for (int i = 0; i < 8; i++) {
+        scanf("%d", &info);
+        if (info == 9) {
+            isCorresponding = true;
+        }
+    }
+    if (isCorresponding) {
+        puts("F");
+    }
+    else {
+        puts("S");
+    }
+}
