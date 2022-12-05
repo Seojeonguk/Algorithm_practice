510f11057ca2f5330e6e492e1f0b306e2ae4b817
Seojeonguk <junguk7880@naver.com>
2022-12-05 21:13:36 +0900

    치킨댄스를 추는 곰곰이를 본 임스 2
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26068.cpp
@@ -0,0 +1,26 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int gifticonCnt, availableGifticonCnt;
+string giftconExpirationDate;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &gifticonCnt);
+    cin.ignore();
+    for (int idx = 0; idx < gifticonCnt; idx++) {
+        getline(cin, giftconExpirationDate);
+        int iExpirationDate = stoi(giftconExpirationDate.substr(2));
+        if (iExpirationDate <= 90) {
+            availableGifticonCnt++;
+        }
+    }
+    printf("%d\n", availableGifticonCnt);
+}
