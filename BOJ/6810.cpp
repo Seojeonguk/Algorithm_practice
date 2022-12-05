7d29bbfc2fc94295ac70d779bd9aa579cdea5897
Seojeonguk <junguk7880@naver.com>
2022-12-05 22:44:38 +0900

    ISBN
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/6810.cpp
@@ -0,0 +1,23 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int sum13 = 91;
+int multiple = 1;
+int lastdigit;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    for (int idx = 0; idx < 3; idx++) {
+        scanf("%d", &lastdigit);
+        sum13 += (lastdigit * multiple);
+        multiple = (multiple + 2) % 4;
+    }
+    printf("The 1-3-sum is %d\n", sum13);
+}
