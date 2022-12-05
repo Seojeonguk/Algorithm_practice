85964a016206552962ae3eb4961ee1a014fa1f37
Seojeonguk <junguk7880@naver.com>
2022-12-05 23:48:30 +0900

    Cupcake Party
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/24568.cpp
@@ -0,0 +1,18 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int regularboxCnt, smallboxCnt;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d", &regularboxCnt, &smallboxCnt);
+    int studentCnt = 28;
+    printf("%d\n", max(0, regularboxCnt * 8 + smallboxCnt * 3 - 28));
+}
