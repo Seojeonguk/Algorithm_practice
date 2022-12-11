4ddad907efe2109a1275f70671c35a63af092a0c
Seojeonguk <junguk7880@naver.com>
2022-12-11 18:27:31 +0900

    알고리즘 수업 - 너비 우선 탐색 1
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/24444.cpp
@@ -0,0 +1,49 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+
+int nodeCnt, lineCnt, startNode;
+int aNode, bNode, cnt;
+vector<int> v[100002];
+int visitOrder[100002];
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d %d", &nodeCnt, &lineCnt, &startNode);
+
+    for (int idx = 0; idx < lineCnt; idx++) {
+        scanf("%d %d", &aNode, &bNode);
+        v[aNode].push_back(bNode);
+        v[bNode].push_back(aNode);
+    }
+
+    for (int idx = 1; idx <= nodeCnt; idx++) {
+        sort(v[idx].begin(), v[idx].end());
+    }
+
+    queue<int> q;
+    q.push(startNode);
+    visitOrder[startNode] = ++cnt;
+    while (!q.empty()) {
+        int now = q.front();
+        q.pop();
+
+        for (int next : v[now]) {
+            if (visitOrder[next]) {
+                continue;
+            }
+            visitOrder[next] = ++cnt;
+            q.push(next);
+        }
+    }
+
+    for (int idx = 1; idx <= nodeCnt; idx++) {
+        printf("%d\n", visitOrder[idx]);
+    }
+}
