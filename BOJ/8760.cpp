2e178954642b5603322ff06ea2bde862b4f087b5
Seojeonguk <junguk7880@naver.com>
2022-12-12 16:09:52 +0900

    Schronisko
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/8760.cpp
@@ -0,0 +1,22 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int tc, width, height;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &tc);
+
+    for (int testCase = 0; testCase < tc; testCase++) {
+        scanf("%d %d", &width, &height);
+        printf("%d\n", width * height / 2);
+    }
+}
