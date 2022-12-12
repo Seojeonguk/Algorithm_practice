6655acd223eb223810f39b35641bc14f9b0bfa7a
Seojeonguk <junguk7880@naver.com>
2022-12-12 15:39:30 +0900

    St. Ives
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/4696.cpp
@@ -0,0 +1,23 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+double processionsSize;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    while (scanf("%lf", &processionsSize) != EOF) {
+        if (processionsSize == 0) {
+            break;
+        }
+        double ans = 1.0f + processionsSize * (processionsSize + 1.0f) * (processionsSize * processionsSize + 1.0f);
+        printf("%.2lf\n", ans);
+    }
+}
