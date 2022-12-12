0bb7698e7f1ded740bd0bfb007ce26013e9ace19
Seojeonguk <junguk7880@naver.com>
2022-12-12 18:18:22 +0900

    트리의 순회
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/2263.cpp
@@ -0,0 +1,41 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+void solve(int, int, int, int);
+int nodeCnt, inOrder[100002], postOrder[100002], inOrderIdx[100002];
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &nodeCnt);
+    for (int idx = 1; idx <= nodeCnt; idx++) {
+        scanf("%d", &inOrder[idx]);
+        inOrderIdx[inOrder[idx]] = idx;
+    }
+    for (int idx = 1; idx <= nodeCnt; idx++) {
+        scanf("%d", &postOrder[idx]);
+    }
+
+    solve(1, nodeCnt, 1, nodeCnt);
+}
+
+void solve(int inLeft,int inRight, int postLeft, int postRight) {
+    if (inLeft > inRight || postLeft > postRight) {
+        return;
+    }
+    
+    printf("%d ", postOrder[postRight]);
+
+    int rootIdx = inOrderIdx[postOrder[postRight]];
+
+    solve(inLeft, rootIdx - 1, postLeft, postLeft + rootIdx - inLeft -1 );
+    solve(rootIdx + 1, inRight, postLeft + rootIdx - inLeft ,postRight-1);
+
+}
