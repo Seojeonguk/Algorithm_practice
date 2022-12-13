144ce8b275495f85a75feb10a503df3053cff908
Seojeonguk <junguk7880@naver.com>
2022-12-13 17:35:35 +0900

    Correct
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26307.cpp
@@ -0,0 +1,19 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int hour, minute;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d", &hour, &minute);
+
+    printf("%d\n", (hour - 9) * 60 + minute);
+}
