14d29f2e3cbfe29553a8c3bb4f94aa08a92f26d0
Seojeonguk <junguk7880@naver.com>
2022-12-13 20:55:46 +0900

    The Merchant of Venice
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/13496.cpp
@@ -0,0 +1,33 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int tc;
+int shipCnt, shipSpeed, date;
+int shipDistance, shipWeight;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &tc);
+
+    for (int testCase = 0; testCase < tc; testCase++) {
+        scanf("%d %d %d", &shipCnt, &shipSpeed, &date);
+        int maximumDistance = shipSpeed * date;
+
+        int sum = 0;
+        for (int ship = 0; ship < shipCnt; ship++) {
+            scanf("%d %d", &shipDistance, &shipWeight);
+            if (shipDistance <= maximumDistance) {
+                sum += shipWeight;
+            }
+        }
+        printf("Data Set %d:\n%d\n\n", testCase + 1, sum);
+    }
+}
