8b7a68aa973e4b530ac5ba46632fe466e53675e0
Seojeonguk <junguk7880@naver.com>
2022-12-13 20:47:54 +0900

    IT Passport Examination
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/11257.cpp
@@ -0,0 +1,28 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int tc;
+int examinee, strategy, management, technology;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &tc);
+
+    for (int testCase = 0; testCase < tc; testCase++) {
+        scanf("%d %d %d %d", &examinee, &strategy, &management, &technology);
+
+        int sum = strategy + management + technology;
+        bool isPass = (strategy > 10 && management > 7 && technology >= 12 && sum >= 55);
+        
+
+        printf("%d %d %s\n", examinee, sum, isPass ? "PASS" : "FAIL");
+    }
+}
