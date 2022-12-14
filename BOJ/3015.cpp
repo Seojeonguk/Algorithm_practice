914b89bc7227d185b07213661b2db8a12cf5858f
Seojeonguk <junguk7880@naver.com>
2022-12-14 18:46:41 +0900

    오아시스 재결합
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/3015.cpp
@@ -0,0 +1,39 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n, height;
+stack<pair<int,int> > s;
+ll ans;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &height);
+
+        while (!s.empty() && s.top().first < height) {
+            ans += s.top().second;
+            s.pop();
+        }
+        
+        int cnt = 1;
+        if (!s.empty() && s.top().first == height) {
+            ans += s.top().second;
+            cnt += s.top().second;
+            s.pop();
+        }
+        if (!s.empty()) ans++;
+        s.push({ height,cnt });
+    }
+
+    printf("%lld\n", ans);
+}
