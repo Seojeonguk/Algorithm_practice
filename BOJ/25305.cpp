e1045854784f14c45646177b706823c3f996ac3a
Seojeonguk <junguk7880@naver.com>
2022-12-16 14:56:59 +0900

    커트라인
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/25305.cpp
@@ -0,0 +1,24 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int studentCnt, k;
+int students[1002];
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d", &studentCnt, &k);
+    for (int idx = 0; idx < studentCnt; idx++)
+        scanf("%d", &students[idx]);
+
+    sort(students, students + studentCnt, greater<>());
+
+    printf("%d\n", students[k - 1]);
+}
