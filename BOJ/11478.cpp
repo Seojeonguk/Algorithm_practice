a81993c43e3dfe8ef8dfa651af989d762490703f
Seojeonguk <junguk7880@naver.com>
2022-12-16 15:51:51 +0900

    서로 다른 부분 문자열의 개수
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/11478.cpp
@@ -0,0 +1,25 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+string str;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    cin >> str;
+    set<string> s;
+    for (int i = 0; i < str.size(); i++) {
+        for (int j = 1; j <= str.size()-i; j++) {
+            string nextstr = str.substr(i, j);
+            s.insert(nextstr);
+        }
+    }
+    cout << s.size() << endl;
+}
