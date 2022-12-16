4b42a96fcf2c11b2e2211a149fff2f8d9256e27d
Seojeonguk <junguk7880@naver.com>
2022-12-16 16:38:59 +0900

    알고리즘 수업 - 피보나치 수 1
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/24416.cpp
@@ -0,0 +1,31 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n,dp[42];
+
+int solve(int x) {
+    int& ret = dp[x];
+    if (x == 1 || x == 2)
+        return 1;
+    if (ret != 0)
+        return ret;
+
+    return ret = solve(x - 1) + solve(x - 2);
+}
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &n);
+
+    solve(n);
+
+    printf("%d %d\n", dp[n], n-2);
+}
