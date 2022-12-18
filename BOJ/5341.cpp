a11c24ee187fc311e6534b99942688475d2e3161
Seojeonguk <junguk7880@naver.com>
2022-12-18 18:00:40 +0900

    Pyramids
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/5341.cpp
@@ -0,0 +1,23 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n;
+int arr[101];
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    for (int i = 1; i < 101; i++)
+        arr[i] += arr[i - 1] + i;
+
+    while (scanf("%d", &n) != EOF && n != 0) {
+        printf("%d\n", arr[n]);
+    }
+}
