bb250a8023608c56af635b7c7388ddf2dba4e126
Seojeonguk <junguk7880@naver.com>
2022-12-18 18:32:24 +0900

    Who is in the middle?
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/6840.cpp
@@ -0,0 +1,22 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int bowls[3];
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    for (int i = 0; i < 3; i++)
+        scanf("%d", &bowls[i]);
+
+    sort(bowls, bowls + 3);
+
+    printf("%d\n", bowls[1]);
+}
