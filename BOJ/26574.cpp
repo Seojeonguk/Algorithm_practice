1ac565bb252ae7a986a7b2b51ef5303ef814ed7e
Seojeonguk <junguk7880@naver.com>
2022-12-18 18:41:20 +0900

    Copier
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26574.cpp
@@ -0,0 +1,22 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n, x, ans;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        printf("%d %d\n", x, x);
+    }
+}
