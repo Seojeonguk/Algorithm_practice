a23ef045b66dea1733fd9be3615e40259b34ea91
Seojeonguk <junguk7880@naver.com>
2022-12-18 18:44:55 +0900

    Pups
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26575.cpp
@@ -0,0 +1,24 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n;
+double d,f,p, ans;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%lf %lf %lf", &d, &f, &p);
+        printf("$%.2lf\n", d * f * p);
+        
+    }
+}
