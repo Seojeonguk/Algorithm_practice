04ea0251f84a318c6ef69f978c9e4ba608f8f19a
Seojeonguk <junguk7880@naver.com>
2022-12-20 00:04:31 +0900

    Dedupe
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/5357.cpp
@@ -0,0 +1,30 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int t;
+char str[102];
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &t);
+    while (t--) {
+        scanf(" %s", str);
+        
+        char recentLetter = 0;
+        for (int idx = 0; idx < strlen(str); idx++) {
+            if (recentLetter != str[idx]) {
+                printf("%c", str[idx]);
+                recentLetter = str[idx];
+            }
+        }
+        puts("");
+    }
+}
