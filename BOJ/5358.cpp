634a80ef013eb39fe2932496756c1cec1ac02d8f
Seojeonguk <junguk7880@naver.com>
2022-12-20 00:24:35 +0900

    Football Team
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/5358.cpp
@@ -0,0 +1,38 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+string players;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    while (1) {
+        if (cin.eof()) {
+            break;
+        }
+        getline(cin,players);
+
+        for (int idx = 0; idx < players.size(); idx++) {
+            if (players[idx] == 'i') {
+                players[idx] = 'e';
+            }
+            else if (players[idx] == 'I') {
+                players[idx] = 'E';
+            }
+            else if (players[idx] == 'e') {
+                players[idx] = 'i';
+            }
+            else if (players[idx] == 'E') {
+                players[idx] = 'I';
+            }
+        }
+        cout << players << '\n';
+    }
+}
