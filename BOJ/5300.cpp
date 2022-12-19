a7a3a384abf8ec0e7150d6274b3cf6cb4bd591f7
Seojeonguk <junguk7880@naver.com>
2022-12-19 23:19:35 +0900

    Fill the Rowboats!
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/5300.cpp
@@ -0,0 +1,26 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", i);
+        if (i % 6 == 0) {
+            printf("Go! ");
+        }
+    }
+    if (n % 6)
+        printf("Go! ");
+}
