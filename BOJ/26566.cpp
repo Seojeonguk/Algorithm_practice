57ba35f2be7354a4983c1a99b9469a6549cf53a2
Seojeonguk <junguk7880@naver.com>
2022-12-21 02:08:02 +0900

    Pizza
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26566.cpp
@@ -0,0 +1,26 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int t;
+int sliceArea, slicePrice, wholeRadius, wholePrice;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &t);
+
+    while (t--) {
+        scanf("%d %d %d %d", &sliceArea, &slicePrice, &wholeRadius, &wholePrice);
+        double slicePerDollar = 1.0f * sliceArea / slicePrice;
+        double wholePerDollar = 1.0f * 3.14159 * wholeRadius * wholeRadius / wholePrice;
+
+        puts(slicePerDollar > wholePerDollar ? "Slice of pizza" : "Whole pizza");
+    }
+}
