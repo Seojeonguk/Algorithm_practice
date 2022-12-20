16bbade9312e2829d5fb0780eb14078af37794a2
Seojeonguk <junguk7880@naver.com>
2022-12-21 00:21:32 +0900

    Terms of Office
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/6888.cpp
@@ -0,0 +1,21 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int todayYear, futureYear;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d", &todayYear, &futureYear);
+
+    for (int year = todayYear; year <= futureYear; year += 60) {
+        printf("All positions change in year %d\n", year);
+    }
+}
