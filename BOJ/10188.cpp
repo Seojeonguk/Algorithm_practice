fcd0aebfa1e75fd2254a2ea1466047bdd0e4fd0b
Seojeonguk <junguk7880@naver.com>
2022-12-21 00:57:12 +0900

    Quadrilateral
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/10188.cpp
@@ -0,0 +1,28 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int tc, r, c;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &tc);
+
+    while (tc--) {
+        scanf("%d %d", &c, &r);
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++)
+                printf("X");
+            puts("");
+        }
+        puts("");
+    }
+}
