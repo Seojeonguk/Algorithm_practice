72fe43bb3b409a0d54243658cfffe5e5b0da86d8
Seojeonguk <junguk7880@naver.com>
2022-12-21 02:21:48 +0900

    Triangle Height
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26592.cpp
@@ -0,0 +1,24 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int t;
+double area, base;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &t);
+
+    while (t--) {
+        scanf("%lf %lf", &area, &base);
+
+        printf("The height of the triangle is %.2lf units\n", area * 2.0 / base);
+    }
+}
