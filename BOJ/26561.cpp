ee16f89dfa5bcc36c2cfd636054db0e580d47741
Seojeonguk <junguk7880@naver.com>
2022-12-21 02:02:58 +0900

    Population
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26561.cpp
@@ -0,0 +1,23 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+ll t, a, b;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%lld", &t);
+
+    while (t--) {
+        scanf("%lld %lld", &a, &b);
+
+        printf("%lld\n", a - b / 7 + b / 4);
+    }
+}
