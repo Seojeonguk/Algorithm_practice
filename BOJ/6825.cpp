35c38bbbc716bc08316366a92476fd157069f0e5
Seojeonguk <junguk7880@naver.com>
2022-12-21 00:16:18 +0900

    Body Mass Index
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/6825.cpp
@@ -0,0 +1,28 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+double height, weight;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%lf %lf", &weight, &height);
+
+    double bmi = weight / (height * height);
+    if (bmi >= 25.0) {
+        puts("Overweight");
+    }
+    else if (bmi >= 18.5) {
+        puts("Normal weight");
+    }
+    else {
+        puts("Underweight");
+    }
+}
