eb3c521b8fec607bda6a73baeb046f281b70e294
Seojeonguk <junguk7880@naver.com>
2022-12-21 01:28:03 +0900

    Simple Sum
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26531.cpp
@@ -0,0 +1,22 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+string str;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    getline(cin, str);
+
+    int a = str[0] - '0';
+    int b = str[4] - '0';
+    int c = str[8] - '0';
+    puts(a + b == c ? "YES" : "NO");
+}
