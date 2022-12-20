39ba7e6358a39e82ec9340e0532364d7e9cf5f30
Seojeonguk <junguk7880@naver.com>
2022-12-21 01:44:57 +0900

    Absolutely
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26500.cpp
@@ -0,0 +1,23 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int t;
+double a, b;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &t);
+
+    while (t--) {
+        scanf("%lf %lf", &a, &b);
+        printf("%.1lf\n", abs(b - a));
+    }
+}
