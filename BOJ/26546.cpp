0fa697dadf47465ca42ff73b9d3de481f94aa7d8
Seojeonguk <junguk7880@naver.com>
2022-12-21 01:23:33 +0900

    Reverse
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26546.cpp
@@ -0,0 +1,23 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+string str;
+int t, i, j;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    cin >> t;
+    while (t--) {
+        cin >> str >> i >> j;
+        str = str.erase(i, j - i);
+        cout << str << '\n';
+    }
+}
