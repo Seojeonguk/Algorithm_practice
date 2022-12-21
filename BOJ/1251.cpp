5cfd68769fd9b70a74d6d540508d1e2553cf20e1
Seojeonguk <junguk7880@naver.com>
2022-12-22 03:16:10 +0900

    단어 나누기
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/1251.cpp
@@ -0,0 +1,31 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+string str;
+priority_queue<string, vector<string>, greater<string> > pq;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    cin >> str;
+    for (int i = 1; i < str.size() - 1; i++) {
+        for (int j = 1; j < str.size() - i; j++) {
+            string first = str.substr(0, i);
+            string second = str.substr(i, j);
+            string third = str.substr(i + j);
+
+            reverse(first.begin(), first.end());
+            reverse(second.begin(), second.end());
+            reverse(third.begin(), third.end());
+            pq.push(first + second + third);
+        }
+    }
+    cout << pq.top() << endl;
+}
