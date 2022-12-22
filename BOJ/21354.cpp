e130221f92a145607f1a51ac7c64e57907ebce5d
Seojeonguk <junguk7880@naver.com>
2022-12-22 19:23:32 +0900

    Äpplen och päron
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/21354.cpp
@@ -0,0 +1,29 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int apple, pear;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d", &apple, &pear);
+
+    apple *= 7;
+    pear *= 13;
+    if (apple > pear) {
+        puts("Axel");
+    }
+    else if (apple < pear) {
+        puts("Petra");
+    }
+    else {
+        puts("lika");
+    }
+}
