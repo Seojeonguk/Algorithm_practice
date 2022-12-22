52cd44152f8165a165debeef6efcfca3b1d00c8c
Seojeonguk <junguk7880@naver.com>
2022-12-23 04:05:10 +0900

    전투의 신
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26595.cpp
@@ -0,0 +1,35 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+ll money;
+ll tanker, dealer;
+ll tankerCost, dealerCost;
+ll ansTanker, ansDealer;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%lld", &money);
+    scanf("%lld %lld", &tanker, &tankerCost);
+    scanf("%lld %lld", &dealer, &dealerCost);
+
+    for (ll nowTankerCnt = 0; nowTankerCnt <= money / tankerCost; nowTankerCnt++) {
+        ll nowDealerCnt = (money - nowTankerCnt * tankerCost) / dealerCost;
+
+        ll power = nowTankerCnt * tanker + nowDealerCnt * dealer;
+        ll ansPower = ansTanker * tanker + ansDealer * dealer;
+        if (ansPower < power) {
+            ansTanker = nowTankerCnt;
+            ansDealer = nowDealerCnt;
+        }
+    }
+
+    printf("%lld %lld\n", ansTanker, ansDealer);
+}
