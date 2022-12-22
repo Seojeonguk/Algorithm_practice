3c6d5ea5ae9ff04279ba1ccde8bdca017cb20ead
Seojeonguk <junguk7880@naver.com>
2022-12-23 03:47:17 +0900

    ZOAC 5
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26594.cpp
@@ -0,0 +1,28 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+char arr[1000002];
+
+int solve() {
+    for (int i = 0; arr[i]; i++) {
+        if (arr[i] != arr[0]) {
+            return i;
+        }
+    }
+    return strlen(arr);
+}
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%s", arr);
+
+    printf("%d\n", solve());
+}
