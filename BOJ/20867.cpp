5123237d9ae7380ce7f99be8d80a873d783b2875
Seojeonguk <junguk7880@naver.com>
2022-12-22 19:20:28 +0900

    Rulltrappa
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/20867.cpp
@@ -0,0 +1,23 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int m, s, g, l, r;
+double a, b;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d %d %lf%lf %d %d", &m, &s, &g, &a, &b, &l, &r);
+
+    double left = 1.0 * l / a + (1.0 * m / g);
+    double right = 1.0 * r / b + (1.0 * m / s);
+
+    puts(left > right ? "latmask" : "friskus");
+}
