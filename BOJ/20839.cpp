887024ded342da1de379e68c3732bdc237e19bce
Seojeonguk <junguk7880@naver.com>
2022-12-22 18:58:49 +0900

    Betygsättning
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/20839.cpp
@@ -0,0 +1,40 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int criterias[3];
+int students[3];
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    for (int i = 0; i < 3; i++) {
+        scanf("%d", &criterias[i]);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        scanf("%d", &students[i]);
+    }
+
+    if (students[1] >= criterias[1]) {
+        if (students[0] >= criterias[0]) {
+            puts("A");
+        }
+        else if (students[0] >= (criterias[0] + 1) / 2) {
+            puts("B");
+        }
+        else {
+            puts("C");
+        }
+    }
+    else if (students[1] >= (criterias[1] + 1) / 2) {
+        puts("D");
+    }
+    else puts("E");
+}
