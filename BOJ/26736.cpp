9ca2c98854feb5c492dc82f3ff69125115a0a4e6
Seojeonguk <junguk7880@naver.com>
2022-12-23 03:36:46 +0900

    Wynik meczu
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26736.cpp
@@ -0,0 +1,25 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int a, b;
+char arr[100002];
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%s", arr);
+
+    for (int i = 0; arr[i]; i++) {
+        if (arr[i] == 'A') a++;
+        else b++;
+    }
+
+    printf("%d : %d\n", a, b);
+}
