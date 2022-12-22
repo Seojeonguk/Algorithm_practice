5ccb04b1d56bac650e709f536f5f4c5ca66ec2d0
Seojeonguk <junguk7880@naver.com>
2022-12-22 18:35:42 +0900

    Zero or One
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/13623.cpp
@@ -0,0 +1,29 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+pair<int,int> zeroOrOne[3];
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    for (int i = 0; i < 3; i++) {
+        scanf("%d", &zeroOrOne[i].first);
+        zeroOrOne[i].second = i;
+    }
+
+    sort(zeroOrOne, zeroOrOne + 3);
+
+    if (zeroOrOne[0].first == zeroOrOne[2].first) {
+        puts("*");
+    }
+    else {
+        printf("%c", zeroOrOne[1].first == 1 ? zeroOrOne[0].second + 'A' : zeroOrOne[2].second + 'A');
+    }
+}
