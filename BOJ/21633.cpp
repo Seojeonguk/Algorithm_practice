9e9d77c89789926f21ca2a94cc044ca6db9b83af
Seojeonguk <junguk7880@naver.com>
2022-12-22 20:02:13 +0900

    Bank Transfer
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/21633.cpp
@@ -0,0 +1,20 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int k;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &k);
+
+    double a = min(2000.0, max(100.0, 1.0 * k / 100.0 + 25));
+    printf("%.2lf\n", a);
+}
