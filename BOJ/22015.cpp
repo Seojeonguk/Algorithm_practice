1be9b4575deeab6d15310bac4039619d0d66e9f8
Seojeonguk <junguk7880@naver.com>
2022-12-22 20:09:11 +0900

    金平糖 (Konpeito)
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/22015.cpp
@@ -0,0 +1,22 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int arr[3];
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    for (int i = 0; i < 3; i++)
+        scanf("%d", &arr[i]);
+
+    sort(arr, arr + 3);
+
+    printf("%d\n", arr[2] - arr[1] + arr[2] - arr[0]);
+}
