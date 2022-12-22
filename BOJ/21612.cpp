640b68188c6c71ecc4931fb39f1655e8f2b5def5
Seojeonguk <junguk7880@naver.com>
2022-12-22 19:31:40 +0900

    Boiling Water
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/21612.cpp
@@ -0,0 +1,21 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int b;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &b);
+
+    int p = 5 * b - 400;
+
+    printf("%d\n%d", p, p == 100 ? 0 : (p > 100 ? -1 : 1));
+}
