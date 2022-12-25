ed6c4b737e921371f1130657fc199817350c033c
Seojeonguk <junguk7880@naver.com>
2022-12-25 23:21:08 +0900

    단순한 문제(Small)
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/25494.cpp
@@ -0,0 +1,23 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int t, a, b, c;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &t);
+
+    while (t--) {
+        scanf("%d %d %d", &a, &b, &c);
+
+        printf("%d\n", min(a, min(b, c)));
+    }
+}
