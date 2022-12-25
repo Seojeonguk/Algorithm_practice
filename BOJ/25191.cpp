12a8e72948c1978463cc210c2f0590a3d65746ac
Seojeonguk <junguk7880@naver.com>
2022-12-25 23:14:58 +0900

    치킨댄스를 추는 곰곰이를 본 임스
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/25191.cpp
@@ -0,0 +1,21 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int chickenCnt, colaCnt, bearCnt;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d %d", &chickenCnt, &colaCnt, &bearCnt);
+
+    int possibleforDrink = colaCnt / 2 + bearCnt;
+    
+    printf("%d\n", min(chickenCnt, possibleforDrink));
+}
