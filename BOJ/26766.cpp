13eaf6606703b69191b46ec20739edfd86475a98
Seojeonguk <junguk7880@naver.com>
2022-12-25 22:48:50 +0900

    Serca
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/26766.cpp
@@ -0,0 +1,29 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &n);
+
+    while (n--) {
+        puts(" @@@   @@@ ");
+        puts("@   @ @   @");
+        puts("@    @    @");
+        puts("@         @");
+        puts(" @       @ ");
+        puts("  @     @  ");
+        puts("   @   @   ");
+        puts("    @ @    ");
+        puts("     @     ");
+    }
+}
