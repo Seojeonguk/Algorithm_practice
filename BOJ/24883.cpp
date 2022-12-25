bdd9c77f71aeffc337dfdc767c966bc2803463a6
Seojeonguk <junguk7880@naver.com>
2022-12-25 23:12:16 +0900

    자동완성
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/24883.cpp
@@ -0,0 +1,23 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+char c;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%c", &c);
+    if (c == 'N' || c == 'n') {
+        puts("Naver D2");
+    }
+    else {
+        puts("Naver Whale");
+    }
+}
