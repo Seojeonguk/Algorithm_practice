35e126dd502f097c81526770ba3babba92b8015b
Seojeonguk <junguk7880@naver.com>
2022-12-27 04:10:48 +0900

    녹색거탑
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/24723.cpp
@@ -0,0 +1,19 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &n);
+
+    printf("%d\n", 1 << n);
+}
