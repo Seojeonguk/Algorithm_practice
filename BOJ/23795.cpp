efc9a128134ec8ed7154f4dd4493884bc944d02a
Seojeonguk <junguk7880@naver.com>
2022-12-27 04:06:07 +0900

    사장님 도박은 재미로 하셔야 합니다
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/23795.cpp
@@ -0,0 +1,25 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int money, total;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    while (1) {
+        scanf("%d", &money);
+
+        if (money == -1) {
+            break;
+        }
+        total += money;
+    }
+    printf("%d\n", total);
+}
