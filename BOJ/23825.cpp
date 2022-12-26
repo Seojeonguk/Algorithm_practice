f951a0975bda78e3076e9b601e90297ec4eae899
Seojeonguk <junguk7880@naver.com>
2022-12-27 04:07:32 +0900

    SASA 모형을 만들어보자
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/23825.cpp
@@ -0,0 +1,19 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int sCnt, aCnt;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d", &sCnt, &aCnt);
+
+    printf("%d\n", min(sCnt / 2, aCnt / 2));
+}
