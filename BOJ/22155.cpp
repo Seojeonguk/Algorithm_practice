d62f77387d755f358fab49ee2e29f0583dd3e19d
Seojeonguk <junguk7880@naver.com>
2022-12-28 21:43:15 +0900

    Простая задача
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/22155.cpp
@@ -0,0 +1,25 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int t, a, b;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &t);
+
+    while (t--) {
+        scanf("%d %d", &a, &b);
+        if ((a <= 2 && b <= 1) || (a <= 1 && b <= 2)) {
+            puts("Yes");
+        }
+        else puts("No");
+    }
+}
