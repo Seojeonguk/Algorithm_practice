6108a30172f2765256a8a37de4aaf150beb56f81
Seojeonguk <junguk7880@naver.com>
2022-12-28 22:34:01 +0900

    アイスクリーム (Ice Cream)
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/24087.cpp
@@ -0,0 +1,19 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int s, a, b;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d %d", &s, &a, &b);
+
+    printf("%d\n", 250 + max((s - a + b - 1) / b * 100,0));
+}
