87cf34364acd6507380db351ea286e41f8fa32e2
Seojeonguk <junguk7880@naver.com>
2022-12-28 22:15:08 +0900

    Arm Coordination
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/23375.cpp
@@ -0,0 +1,22 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+ll x, y, r;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%lld %lld %lld", &x, &y, &r);
+
+    printf("%lld %lld\n", x - r, y - r);
+    printf("%lld %lld\n", x - r, y + r);
+    printf("%lld %lld\n", x + r, y + r);
+    printf("%lld %lld\n", x + r, y - r);
+}
