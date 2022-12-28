d5fb1ff642adbe7149a4d06548574e5261c76559
Seojeonguk <junguk7880@naver.com>
2022-12-28 22:17:59 +0900

    帰省 (Homecoming)
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/24072.cpp
@@ -0,0 +1,19 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int a, b, c;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d %d", &a, &b, &c);
+
+    puts(a <= c && c < b ? "1" : "0");
+}
