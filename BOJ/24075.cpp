b69bea74cfc56f181db1d6cc21929a5642acd4ea
Seojeonguk <junguk7880@naver.com>
2022-12-28 22:20:32 +0900

    計算 (Calculation)
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/24075.cpp
@@ -0,0 +1,19 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int a, b;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d", &a, &b);
+
+    printf("%d\n%d\n", max(a + b, a - b), min(a + b, a - b));
+}
