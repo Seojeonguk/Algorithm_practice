fb7b462e90a742bcd5e1f6e301d868c650e4223e
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2022-12-30 21:37:20 +0900

    Corona Virus Testing

---
+++ BOJ/25828.cpp
@@ -0,0 +1,22 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int a, b, c;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d %d", &a, &b, &c);
+
+    int each = a * b;
+    int group = a + b * c;
+
+    puts(each == group ? "0" : (each > group ? "2" : "1"));
+}
