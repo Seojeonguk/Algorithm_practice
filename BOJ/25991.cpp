dd87b0cc881e574ebd3c60ecf285a5903eb61f4b
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2022-12-30 22:55:42 +0900

    Lots of Liquid

---
+++ BOJ/25991.cpp
@@ -0,0 +1,26 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int t;
+double total, a;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &t);
+
+    while (t--) {
+        scanf("%lf", &a);
+
+        total += a * a * a;
+    }
+
+    printf("%lf\n", cbrt(total));
+}
