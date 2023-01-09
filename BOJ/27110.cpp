f4c13f479736cc5e10fe57436b7f19d2636b059f
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-01-09 17:30:37 +0900

    특식 배부

---
+++ BOJ/27110.cpp
@@ -0,0 +1,23 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n, ans, x;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &n);
+
+    for (int i = 0; i < 3; i++) {
+        scanf("%d", &x);
+        ans += min(n, x);
+    }
+    printf("%d\n", ans);
+}
