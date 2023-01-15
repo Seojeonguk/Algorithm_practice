7a0be744c142f361638e38ac25a5ab80164ac530
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-01-16 08:54:13 +0900

    Робинзон Крузо

---
+++ BOJ/27219.cpp
@@ -0,0 +1,23 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &n);
+
+    for (int i = 0; i < n / 5; i++)
+        printf("V");
+
+    for (int i = 0; i < n % 5; i++)
+        printf("I");
+}
