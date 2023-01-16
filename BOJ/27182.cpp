fe43ca87f3754ad0a88c59204ef2e6905cb11b33
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-01-16 10:58:46 +0900

    Rain Diary

---
+++ BOJ/27182.cpp
@@ -0,0 +1,19 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n, m;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d", &n, &m);
+
+    printf("%d\n", n > 7 ? n - 7 : m + 7);
+}
