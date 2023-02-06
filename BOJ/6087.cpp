c21da224e64705f78eb292e095c4b9a9568080e9
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-02-07 04:27:24 +0900

    레이저 통신

--- BOJ/6087.cpp
+++ BOJ/6087.cpp
@@ -1,4 +1,4 @@
-﻿#ifdef _DEBUG
+#ifdef _DEBUG
 #include "bits_stdc++.h"
 #else
 #include "bits/stdc++.h"
@@ -10,15 +10,8 @@ bool isOut(int, int);
 int rowSize, colSize;
 char maps[102][102];
 int cost[102][102];
-struct laser {
-    int turnCnt, x, y, dir;
-};
-struct compare {
-    bool operator()(laser& a, laser& b) {
-        return a.turnCnt > b.turnCnt;
-    }
-};
-priority_queue<laser, vector<laser>, compare> pq;
+int x = -1, y = -1;
+queue<pair<int,int> > q;
 int X[] = { -1,1,0,0 }, Y[] = { 0,0,-1,1 };
 
 int main() {
@@ -29,37 +22,32 @@ int main() {
     for (int row = 0; row < rowSize; row++) {
         scanf(" %s", &maps[row]);
         for (int col = 0; col < colSize; col++) {
-            cost[row][col] = INT32_MAX;
-            if (maps[row][col] == 'C' && pq.empty()) {
-                cost[row][col] = 0;
-                pq.push({ -1,row,col,-1 });
-                maps[row][col] = '.';
+            if (maps[row][col] == 'C') {
+                if (y == -1) x = row, y = col;
+                else q.push({ row,col });
             }
         }
     }
-    while (!pq.empty()) {
-        laser now = pq.top();
-        pq.pop();
-
-        if (maps[now.x][now.y] == 'C') {
-            printf("%d\n", now.turnCnt);
-            return 0;
-        }
+    while (!q.empty()) {
+        pair<int, int> now = q.front();
+        q.pop();
 
         for (int dir = 0; dir < 4; dir++) {
-            int nextRow = now.x + X[dir];
-            int nextCol = now.y + Y[dir];
-            int nextTurnCnt = now.turnCnt + (now.dir != dir);
-            int nextDir = dir;
+            int nextRow = now.first + X[dir];
+            int nextCol = now.second + Y[dir];
 
-            if (isOut(nextRow, nextCol) || nextTurnCnt > cost[nextRow][nextCol] || maps[nextRow][nextCol] == '*') {
-                continue;
+            while (!isOut(nextRow, nextCol) && maps[nextRow][nextCol] != '*') {
+                if (!cost[nextRow][nextCol]) {
+                    cost[nextRow][nextCol] = cost[now.first][now.second] + 1;
+                    q.push({ nextRow,nextCol });
+                }
+                nextRow += X[dir];
+                nextCol += Y[dir];
             }
-
-            cost[nextRow][nextCol] = nextTurnCnt;
-            pq.push({ nextTurnCnt,nextRow,nextCol,nextDir });
         }
     }
+
+    printf("%d\n", cost[x][y] - 1);
 }
 
 bool isOut(int x, int y) {
