e8f5d07b30fb82efb23614d21ce7304cb0260f3a
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-02-20 16:21:27 +0900

    치킨 먹고 싶다

--- BOJ/13199.cpp
+++ BOJ/13199.cpp
@@ -1,16 +1,35 @@
-#include <iostream>
-
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
 using namespace std;
+typedef long long ll;
 
-long long T, P, M, F, C;
+int t;
+ll price, money, require, add_coupon;
 
-int main(void) {
-	ios_base::sync_with_stdio(false), cin.tie(nullptr);
-	cin >> T;
-	while(T--) {
-		cin >> P >> M >> F >> C;
-		cout << (M/P*C > F ? (M/P*C - F)/(F - C) - M/P*C/F + 1: 0)<< '\n';
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &t);
+
+	while (t--) {
+		scanf("%lld %lld %lld %lld", &price, &money, &require, &add_coupon);
+
+		ll coupon = (money / price) * add_coupon;
+
+		coupon = (coupon % require) + ((coupon / require) * add_coupon);
+
+		coupon -= require;
+
+		ll ans = 0;
+
+		if (coupon >= 0)
+			ans = 1 + coupon / (require - add_coupon);
+
+		printf("%lld\n", ans);
 	}
-	
-	return 0;
 }
