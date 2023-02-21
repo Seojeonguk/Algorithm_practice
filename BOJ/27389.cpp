bb46eadf6e30780e1eed9f1d8316ae9433cb1d2a
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-02-21 23:52:58 +0900

    Metronome

---
+++ BOJ/27389.cpp
@@ -0,0 +1,19 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n;
+
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &n);
+
+	printf("%0.2lf\n", n / 4.0);
+}
