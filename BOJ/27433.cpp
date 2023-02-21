2f54d8d16f645d281e65a94722b2b9be38d3d40a
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-02-21 23:44:30 +0900

    팩토리얼 2

---
+++ BOJ/27433.cpp
@@ -0,0 +1,23 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n;
+ll ans = 1;
+
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+		ans *= i;
+
+	printf("%lld\n", ans);
+} 
