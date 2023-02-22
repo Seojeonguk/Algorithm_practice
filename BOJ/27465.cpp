f2d438b2c76b91138611e5247ab25215a243a3c7
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-02-23 00:36:22 +0900

    소수가 아닌 수

---
+++ BOJ/27465.cpp
@@ -0,0 +1,16 @@
+#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+
+int main() {
+#ifdef _CONSOLE 
+	freopen("sample.txt", "r", stdin);
+#endif
+	puts("1000000000");
+}
