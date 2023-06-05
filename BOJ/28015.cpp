5501b49ca7fe50c1ab24573cddcf399e36add61a
Seojeonguk <junguk7880@naver.com>
2023-06-05 16:41:23 +0900

    영역 색칠
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/28015.cpp
@@ -0,0 +1,35 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n, m, ans, paintings[102][102];
+int cnt[3];
+
+void cal() {
+    if (!cnt[1] && !cnt[2]) return;
+    ans += min(cnt[1], cnt[2]) + 1;
+    cnt[1] = cnt[2] = 0;
+}
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &paintings[i][j]);
+            if (paintings[i][j])  cnt[paintings[i][j]] += (paintings[i][j] != paintings[i][j - 1] ? 1 : 0);
+            else cal();
+        }
+        cal();
+    }
+
+    printf("%d\n", ans);
+}
