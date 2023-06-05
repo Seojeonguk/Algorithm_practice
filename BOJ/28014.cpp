3e1da9a7d24ce9847a86d6e111f199ff6b232276
Seojeonguk <junguk7880@naver.com>
2023-06-05 11:44:37 +0900

    첨탑 밀어서 부수기
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/28014.cpp
@@ -0,0 +1,27 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n, ans, h, prev_h;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &n);
+
+    while (n--) {
+        scanf("%d", &h);
+        if (prev_h <= h) {
+            ans++;
+        }
+        prev_h = h;
+    }
+
+    printf("%d\n", ans);
+}
