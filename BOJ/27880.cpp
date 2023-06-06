a312993483173cb104d2f9d6c3e0a7a0fc1b6a84
Seojeonguk <junguk7880@naver.com>
2023-06-06 22:12:27 +0900

    Gahui and Soongsil University station
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/27880.cpp
@@ -0,0 +1,23 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+char adjacent[8];
+int h, ans;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    for(int i=0;i<4;i++) {
+        scanf("%s %d", adjacent, &h);
+        ans += h * (adjacent[0] == 'E' ? 21 : 17);
+    }
+
+    printf("%d\n", ans);
+}
