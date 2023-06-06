f8080d3030d0703e1bedd597b755aa76917e22e5
Seojeonguk <junguk7880@naver.com>
2023-06-06 22:34:37 +0900

    Cornhole
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/27855.cpp
@@ -0,0 +1,23 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+pair<int, int> hb[2];
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    for (int i = 0; i < 2; i++)
+        scanf("%d %d", &hb[i].first, &hb[i].second);
+
+    int fp = hb[0].first * 3 + hb[0].second;
+    int sp = hb[1].first * 3 + hb[1].second;
+    if (fp == sp) puts("NO SCORE");
+    else printf("%d %d\n", fp < sp ? 2 : 1, abs(fp - sp));
+}
