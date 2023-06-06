e06192931c03dafb65a4119d0858c9f2f0c8aeae
Seojeonguk <junguk7880@naver.com>
2023-06-06 22:22:31 +0900

    모비스
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/28074.cpp
@@ -0,0 +1,23 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+char a[102];
+bool cnt[26];
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%s", a);
+
+    for (int i = 0; a[i]; i++)
+        cnt[a[i] - 'A'] = true;
+
+    puts((cnt['M' - 'A'] && cnt['O' - 'A'] && cnt['B' - 'A'] && cnt['I' - 'A'] && cnt['S' - 'A']) ? "YES" : "NO");
+}
