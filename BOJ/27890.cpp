3007af61b375a3ff9fdce584bb10bbf3a0aa156e
Seojeonguk <junguk7880@naver.com>
2023-06-06 22:29:32 +0900

    특별한 작은 분수
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/27890.cpp
@@ -0,0 +1,24 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int x, n;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d", &x, &n);
+
+    for (int i = 1; i <= n; i++) {
+        if (x & 1) x = (2 * x) ^ 6;
+        else x = (x / 2) ^ 6;
+    }
+
+    printf("%d\n", x);
+}
