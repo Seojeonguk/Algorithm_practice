0a5761b5885fc75609221748ec649dc1a922d8cf
Seojeonguk <junguk7880@naver.com>
2023-06-06 22:26:03 +0900

    모범생 포닉스
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/28097.cpp
@@ -0,0 +1,25 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n, t, total;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &t);
+        total += t;
+    }
+    total += (n - 1) * 8;
+
+    printf("%d %d\n", total / 24, total % 24);
+}
