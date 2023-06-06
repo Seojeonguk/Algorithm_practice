e200649fe59f1b74b85ca2eccf81b8d2ca5aec65
Seojeonguk <junguk7880@naver.com>
2023-06-06 21:13:47 +0900

    Pokemon Buddy
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/18691.cpp
@@ -0,0 +1,24 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int t, g, c, e;
+int dis[4] = { 0,1,3,5 };
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &t);
+
+    while (t--) {
+        scanf("%d %d %d", &g, &c, &e);
+
+        printf("%d\n", c < e ? (e - c) * dis[g] : 0);
+    }
+}
