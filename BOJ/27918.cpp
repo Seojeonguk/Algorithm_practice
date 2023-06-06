cc05c25ca721a3fe4be26ba1842738aacd304ddf
Seojeonguk <junguk7880@naver.com>
2023-06-06 22:16:27 +0900

    탁구 경기
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/27918.cpp
@@ -0,0 +1,28 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int n, x, y;
+char c;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf(" %c", &c);
+        if (c == 'D') x++;
+        else y++;
+
+        if (abs(x - y) == 2) break;
+    }
+
+    printf("%d:%d\n", x, y);
+}
