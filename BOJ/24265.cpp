b547f75a5a49b4c7b32f9508d09ebb8eb42c4fb6
Seojeonguk <junguk7880@naver.com>
2023-06-13 09:14:15 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 4
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/24265.cpp
@@ -0,0 +1,19 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+ll n;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%lld", &n);
+
+    printf("%lld\n%d\n", n * (n - 1) / 2, 2);
+}
