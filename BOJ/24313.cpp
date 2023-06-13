d8924bc6c70c3cf668276f75c7c7067b18c90c4b
Seojeonguk <junguk7880@naver.com>
2023-06-13 09:54:59 +0900

    알고리즘 수업 - 점근적 표기 1
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/24313.cpp
@@ -0,0 +1,22 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+int a1, a2, c, n0;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%d %d %d %d", &a1, &a2, &c, &n0);
+
+    int firstFn = a1 * n0 + a2;
+    int firstGn = c * n0;
+
+    puts(firstFn <= firstGn && a1 <= c ? "1" : "0");
+}
