701ca3f7d41b4d784671dd5464a08572da79f332
Seojeonguk <junguk7880@naver.com>
2023-06-13 10:13:41 +0900

    창문 닫기
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/13909.cpp
@@ -0,0 +1,23 @@
+﻿#ifdef _DEBUG
+#include "bits_stdc++.h"
+#else
+#include "bits/stdc++.h"
+#endif
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+
+ll n;
+int ans;
+
+int main() {
+#ifdef _CONSOLE 
+    freopen("sample.txt", "r", stdin);
+#endif
+    scanf("%lld", &n);
+
+    for (ll i = 1; i * i <= n; i++)
+        ans++;
+
+    printf("%d\n", ans);
+}
