1290844efd98877d05115f44350d478cbad5eae2
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-08-21 18:41:41 +0900

    단어순서 뒤집기

---
+++ BOJ/12605.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        int testCase = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < testCase; tc++) {
+            String[] sentences = br.readLine().split(" ");
+            sb.append(String.format("Case #%d: ", tc + 1));
+            for (int idx = sentences.length - 1; idx >= 0; idx--) {
+                sb.append(sentences[idx]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
