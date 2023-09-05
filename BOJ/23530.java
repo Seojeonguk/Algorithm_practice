ad1ae83fd51e456536c3f8e6c7e7ce0db8db6671
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-09-05 16:59:10 +0900

    Not A + B

---
+++ BOJ/23530.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        int testCase = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < testCase; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(1).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
