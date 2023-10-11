d7cb23cf841e1e5317bb7d10f682fc4286e595a7
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-10-11 16:05:11 +0900

    골뱅이 찍기 - ㅁ

---
+++ BOJ/23806.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n * 5; i++) {
+            for (int j = 0; j < n * 5; j++) {
+                sb.append((i >= n && j >= n && i < n * 4 && j < n * 4) ? " " : "@");
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
