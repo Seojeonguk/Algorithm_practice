6c310a795b79facb17f507c248ecd7fdff506ac2
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-10-29 23:07:43 +0900

    Periods

---
+++ BOJ/26560.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        int lineCnt = Integer.parseInt(br.readLine());
+
+        while (lineCnt-- > 0) {
+            String line = br.readLine();
+            sb.append(line);
+            if (!line.endsWith(".")) {
+                sb.append(".");
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
