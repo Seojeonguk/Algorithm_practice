edd839f6302ff8f7d2030803a8254f309b6cd795
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-12-16 12:09:57 +0900

    Космический корабль

---
+++ BOJ/29002.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        int totalEnemy = Integer.parseInt(br.readLine());
+        long[] enemyPowers = new long[totalEnemy];
+        st = new StringTokenizer(br.readLine());
+        long totalPower = 0;
+        for (int i = 0; i < totalEnemy; i++) {
+            enemyPowers[i] = Long.parseLong(st.nextToken());
+            totalPower += enemyPowers[i];
+        }
+
+        int boss = -1;
+        for (int i = 0; i < totalEnemy; i++) {
+            if (totalPower - enemyPowers[i] == enemyPowers[i]) {
+                boss = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < totalEnemy; i++) {
+            if (i == boss) {
+                continue;
+            }
+            sb.append(enemyPowers[i]).append(" ");
+        }
+        sb.append(enemyPowers[boss]);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
