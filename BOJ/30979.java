593c795c5e263c236f835830b04d2c1176daf993
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-12-20 21:06:14 +0900

    유치원생 파댕이 돌보기

---
+++ BOJ/30979.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        int time = Integer.parseInt(br.readLine());
+        int totalCandy = Integer.parseInt(br.readLine());
+        int padaengNotCryingTime = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sum();
+        if (padaengNotCryingTime >= time) {
+            sb.append("Padaeng_i Happy");
+        } else {
+            sb.append("Padaeng_i Cry");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
