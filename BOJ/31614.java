3afce6ce0c312cd97c04532cb7c39236dbea191e
Seojeonguk <junguk7880@naver.com>
2024-03-13 15:33:01 +0900

    分 (Minutes)

---
+++ BOJ/31614.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int hour = Integer.parseInt(br.readLine());
+        int minute = Integer.parseInt(br.readLine());
+
+        sb.append(hour * 60 + minute);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
