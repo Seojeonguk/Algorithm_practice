ca07e31843000ebab6e04c6d47b09da036cb9001
Seojeonguk <junguk7880@naver.com>
2024-03-19 18:14:32 +0900

    Ezreal 여눈부터 가네 ㅈㅈ

---
+++ BOJ/20500.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    static final int MOD = 1000000007;
+
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n + 1][3];
+        dp[1][2] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = (dp[i - 1][1] + dp[i - 1][2]) % MOD;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+        }
+
+        sb.append(dp[n][0]);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
