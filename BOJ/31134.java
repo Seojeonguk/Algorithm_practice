6c8d44324f9fc08e7346826c6e5a901ba8431a80
Seojeonguk <junguk7880@naver.com>
2024-06-17 19:08:54 +0900

    A Hero Named Magnus

---
+++ BOJ/31134.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+             BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder sb = new StringBuilder();
+
+            int testCase = Integer.parseInt(br.readLine());
+            for (int tc = 0; tc < testCase; tc++) {
+                int n = Integer.parseInt(br.readLine());
+
+                sb.append(2L * n - 1).append("\n");
+            }
+
+            bw.write(sb.toString());
+        }
+    }
+}
