f25606f68e1c7202e4c138427c3f792f85ac4d19
Seojeonguk <junguk7880@naver.com>
2024-11-26 23:53:43 +0900

    Dusa And The Yobis
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/32327.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+             BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder sb = new StringBuilder();
+
+            int currentSize = Integer.parseInt(br.readLine());
+
+            while (true) {
+                int targetSize = Integer.parseInt(br.readLine());
+
+                if (currentSize <= targetSize) {
+                    break;
+                }
+
+                currentSize += targetSize;
+            }
+
+            sb.append(currentSize);
+
+            bw.write(sb.toString());
+        }
+    }
+}
