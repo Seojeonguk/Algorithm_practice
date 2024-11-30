b5b973e29135174b3d57455a7d42cb3833f73f07
Seojeonguk <junguk7880@naver.com>
2024-12-01 00:35:31 +0900

    문자열을 만들어요
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/32297.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+             BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder sb = new StringBuilder();
+
+            int wordLength = Integer.parseInt(br.readLine());
+            String word = br.readLine();
+
+            sb.append(word.contains("gori") ? "YES" : "NO");
+
+            bw.write(sb.toString());
+        }
+    }
+}
