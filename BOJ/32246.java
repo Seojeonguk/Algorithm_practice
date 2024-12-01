6d241031750d71b1f7bb5d628e237dd10482d954
Seojeonguk <junguk7880@naver.com>
2024-12-02 00:29:23 +0900

    鍮숆퀬 留됯린
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/32246.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+             BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder sb = new StringBuilder();
+
+            int length = Integer.parseInt(br.readLine());
+
+            sb.append(length == 2 ? 3 : length);
+
+            bw.write(sb.toString());
+        }
+    }
+}
