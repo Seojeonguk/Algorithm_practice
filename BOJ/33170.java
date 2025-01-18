beb99a2e3b407d3e6641a4de71baa9cd521c4ae6
Seojeonguk <junguk7880@naver.com>
2025-01-18 23:59:50 +0900

    ブラックジャック (Blackjack)
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/33170.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static final int LIMIT = 21;
+
+    public static void main(String[] args) throws Exception {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+             BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder sb = new StringBuilder();
+
+            int a = Integer.parseInt(br.readLine());
+            int b = Integer.parseInt(br.readLine());
+            int c = Integer.parseInt(br.readLine());
+
+            sb.append(a + b + c <= LIMIT ? 1 : 0);
+
+            bw.write(sb.toString());
+        }
+    }
+}
