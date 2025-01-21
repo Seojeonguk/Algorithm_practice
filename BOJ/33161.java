b92456277360df3344ea96627609a8c02eabfa7f
Seojeonguk <junguk7880@naver.com>
2025-01-21 23:54:18 +0900

    鉛筆 2 (Pencils 2)
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/33161.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+             BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder sb = new StringBuilder();
+
+            int money = Integer.parseInt(br.readLine());
+
+            sb.append(money / 5);
+
+            bw.write(sb.toString());
+        }
+    }
+}
