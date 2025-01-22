b707f775764240a24fe2e3e94b9674f5d7f3905e
Seojeonguk <junguk7880@naver.com>
2025-01-23 00:17:59 +0900

    Consolidating Windows
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ BOJ/32888.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+             BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder sb = new StringBuilder();
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+
+            long sum = a * a + b * b;
+            sb.append(String.format("%.15f", Math.sqrt(sum)));
+
+            bw.write(sb.toString());
+        }
+    }
+}
