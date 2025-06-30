e57ad5b435b5e8fe6972c8c3e7b3e3eda158b04d
Seojeonguk <junguk7880@naver.com>
2025-07-01 00:21:19 +0900

    새천년관

---
+++ BOJ/33701.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+             BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("nkugwan");
+
+            bw.write(sb.toString());
+        }
+    }
+}
