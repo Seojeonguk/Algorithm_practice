da2df0ab8d2ee589114fdd0f11ce375eff6694de
Seojeonguk <junguk7880@naver.com>
2026-01-10 21:14:55 +0900

    얼룩말과 사자

---
+++ BOJ/34759.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        long tiger = Long.parseLong(br.readLine());
+        sb.append(tiger * 2);
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
