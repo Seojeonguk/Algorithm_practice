7c991d28d2130ae80f7b2541683c01b0fa449c67
Seojeonguk <junguk7880@naver.com>
2026-03-09 17:55:34 +0900

    Memories of Passport Control

---
+++ BOJ/35370.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int page = Integer.parseInt(st.nextToken());
+        int totalPage = Integer.parseInt(st.nextToken());
+
+        sb.append(totalPage / page + totalPage % page);
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
