4128f6ec121f0c8a98b3ea9253fc91453eca42d6
Seojeonguk <junguk7880@naver.com>
2026-04-30 00:25:47 +0900

    안전지대

---
+++ Programmers/안전지대.js
@@ -0,0 +1,36 @@
+class Solution {
+    public int solution(int[][] board) {
+        int n = board.length;
+        boolean[][] danger = new boolean[n][n];
+
+        int[] dx = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) {
+                    for (int k = 0; k < 9; k++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+
+                        if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                            danger[nx][ny] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!danger[i][j]) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
