9fd50ceb85c35bd6aaf988d02c00f6417b2bb076
Seojeonguk <junguk7880@naver.com>
2026-04-30 00:24:25 +0900

    다음에 올 숫자

---
+++ Programmers/다음에 올 숫자.js
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] common) {
+        
+        int diff1 = common[1] - common[0];
+        int diff2 = common[2] - common[1];
+        
+        int last = common[common.length - 1];
+        
+        if (diff1 == diff2) {
+            return last + diff1;
+        } else {
+            int ratio = common[1] / common[0];
+            return last * ratio;
+        }
+    }
+}
