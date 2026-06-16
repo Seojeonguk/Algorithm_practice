de866ec7628f6669461362e65d16bc0339bb9dd9
Seojeonguk <junguk7880@naver.com>
2026-06-16 18:56:06 +0900

    홀짝에 따라 다른 값 반환하기

---
+++ Programmers/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int ret = 0;
+        if(n % 2 == 0) {
+            for(int i = 0; i <= n; i += 2) {
+                ret += i * i;
+            }
+        } else {
+            for(int i = 1; i <= n; i += 2) {
+                ret += i;
+            }
+        }
+        
+        return ret;
+    }
+}
