05534057360aa97a8fba621af7ef882c9bfa49ec
Seojeonguk <junguk7880@naver.com>
2026-06-23 15:22:34 +0900

    배열 비교하기

---
+++ Programmers/배열 비교하기.js
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) {
+            return Integer.compare(arr1.length, arr2.length);
+        }
+
+        int arr1Sum = Arrays.stream(arr1).sum();
+        int arr2Sum = Arrays.stream(arr2).sum();
+        
+        return Integer.compare(arr1Sum, arr2Sum);
+    }
+}
