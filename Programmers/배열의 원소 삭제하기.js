36a0e6e539591f5a926a027bbf396be1efaba55e
Seojeonguk <junguk7880@naver.com>
2026-08-04 11:37:24 +0900

    배열의 원소 삭제하기

---
+++ Programmers/배열의 원소 삭제하기.js
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[] delete_list) {
+        return Arrays.stream(arr)
+                .filter(x -> Arrays.stream(delete_list).noneMatch(y -> x == y))
+                .toArray();
+    }
+}
