a01e0bcf6af161b7bcaddcf44d9ba88e389696d7
Seojeonguk <junguk7880@naver.com>
2026-08-07 13:24:55 +0900

    커피 심부름

---
+++ Programmers/커피 심부름.js
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String[] order) {
+        int sum = 0;
+        for (String o : order) {
+            if (o.contains("cafelatte")) {
+                sum += 5000;
+            } else {
+                sum += 4500;
+            }
+        }
+            
+        return sum;
+    }
+}
